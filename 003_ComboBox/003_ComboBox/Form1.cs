d20e18c7364aac99fb31a2e33c7c413aeb506099
john <jm_sasuke25@yahoo.com>
2018-01-02 20:56:27 +0900

    project 003

---
+++ 003_ComboBox/003_ComboBox/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _003_ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // event handler index change 
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {   
+            // get to the item of combo box set to the label lblValue
+            lblValue.Text = comboBox1.Text;
+        }
+        // add item to the combobox in the loadout 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Arizoma");
+            comboBox1.Items.Add("Osaka");
+            // clear the item combobox2
+            comboBox2.Items.Clear();
+            // Init Data
+            List<State> list = new List<State>();
+            list.Add(new State() { ID = "01", Name = "Celtic" });
+            list.Add(new State() { ID = "02", Name = "Miami" });
+            list.Add(new State() { ID = "03", Name = "Phili" });
+            list.Add(new State() { ID = "04", Name = "LA" });
+            list.Add(new State() { ID = "05", Name = "Brocklyn" });
+            // set Display member and value for combobox2
+            comboBox2.DataSource = list;
+            comboBox2.ValueMember = "ID";
+            comboBox2.DisplayMember = "Name";
+        }
+
+        private void comboBox2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            State obj = comboBox2.SelectedItem as State;
+            if(obj != null)
+            {
+                lblValue.Text = obj.Name;
+            }
+        }
+    }
+}
---
+++ 003_ComboBox/003_ComboBox/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_ComboBox
+{
+     public class State
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
