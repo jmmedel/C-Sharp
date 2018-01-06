36f223307299efa7547e73f9aa775f7e7187e95b
john <jm_sasuke25@yahoo.com>
2018-01-06 13:02:52 +0900

    Not Done

---
+++ 016_Export_DataGridView_To_Text_File/016_Export_DataGridView_To_Text_File/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _016_Export_DataGridView_To_Text_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
