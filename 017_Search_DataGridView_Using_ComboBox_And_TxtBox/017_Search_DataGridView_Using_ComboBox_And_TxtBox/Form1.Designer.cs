cfc0a1d87646c022f90752ce7d7b083c142c20e6
john <jm_sasuke25@yahoo.com>
2018-01-08 15:21:28 +0900

    project 017

--- 017_Search_DataGridView_Using_ComboBox_And_TxtBox/017_Search_DataGridView_Using_ComboBox_And_TxtBox/Form1.Designer.cs
+++ 017_Search_DataGridView_Using_ComboBox_And_TxtBox/017_Search_DataGridView_Using_ComboBox_And_TxtBox/Form1.Designer.cs
@@ -28,12 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.cbColumn = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.cbSearch = new System.Windows.Forms.ComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.storidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ordnumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orddateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qtyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.paytermsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.saleData = new _017_Search_DataGridView_Using_ComboBox_And_TxtBox.SaleData();
+            this.salesTableAdapter = new _017_Search_DataGridView_Using_ComboBox_And_TxtBox.SaleDataTableAdapters.salesTableAdapter();
+            this.txtSearch = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saleData)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -48,6 +60,13 @@
             // cbColumn
             // 
             this.cbColumn.FormattingEnabled = true;
+            this.cbColumn.Items.AddRange(new object[] {
+            "stor_id",
+            "ord_num",
+            "ord_date",
+            "qty",
+            "payterms",
+            "title_id"});
             this.cbColumn.Location = new System.Drawing.Point(72, 32);
             this.cbColumn.Name = "cbColumn";
             this.cbColumn.Size = new System.Drawing.Size(218, 20);
@@ -62,38 +81,100 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Search";
             // 
-            // cbSearch
-            // 
-            this.cbSearch.FormattingEnabled = true;
-            this.cbSearch.Location = new System.Drawing.Point(354, 32);
-            this.cbSearch.Name = "cbSearch";
-            this.cbSearch.Size = new System.Drawing.Size(164, 20);
-            this.cbSearch.TabIndex = 3;
-            // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(25, 58);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.storidDataGridViewTextBoxColumn,
+            this.ordnumDataGridViewTextBoxColumn,
+            this.orddateDataGridViewTextBoxColumn,
+            this.qtyDataGridViewTextBoxColumn,
+            this.paytermsDataGridViewTextBoxColumn,
+            this.titleidDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.salesBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 58);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 21;
             this.dataGridView1.Size = new System.Drawing.Size(493, 309);
             this.dataGridView1.TabIndex = 4;
+            // 
+            // storidDataGridViewTextBoxColumn
+            // 
+            this.storidDataGridViewTextBoxColumn.DataPropertyName = "stor_id";
+            this.storidDataGridViewTextBoxColumn.HeaderText = "stor_id";
+            this.storidDataGridViewTextBoxColumn.Name = "storidDataGridViewTextBoxColumn";
+            // 
+            // ordnumDataGridViewTextBoxColumn
+            // 
+            this.ordnumDataGridViewTextBoxColumn.DataPropertyName = "ord_num";
+            this.ordnumDataGridViewTextBoxColumn.HeaderText = "ord_num";
+            this.ordnumDataGridViewTextBoxColumn.Name = "ordnumDataGridViewTextBoxColumn";
+            // 
+            // orddateDataGridViewTextBoxColumn
+            // 
+            this.orddateDataGridViewTextBoxColumn.DataPropertyName = "ord_date";
+            this.orddateDataGridViewTextBoxColumn.HeaderText = "ord_date";
+            this.orddateDataGridViewTextBoxColumn.Name = "orddateDataGridViewTextBoxColumn";
+            // 
+            // qtyDataGridViewTextBoxColumn
+            // 
+            this.qtyDataGridViewTextBoxColumn.DataPropertyName = "qty";
+            this.qtyDataGridViewTextBoxColumn.HeaderText = "qty";
+            this.qtyDataGridViewTextBoxColumn.Name = "qtyDataGridViewTextBoxColumn";
+            // 
+            // paytermsDataGridViewTextBoxColumn
+            // 
+            this.paytermsDataGridViewTextBoxColumn.DataPropertyName = "payterms";
+            this.paytermsDataGridViewTextBoxColumn.HeaderText = "payterms";
+            this.paytermsDataGridViewTextBoxColumn.Name = "paytermsDataGridViewTextBoxColumn";
+            // 
+            // titleidDataGridViewTextBoxColumn
+            // 
+            this.titleidDataGridViewTextBoxColumn.DataPropertyName = "title_id";
+            this.titleidDataGridViewTextBoxColumn.HeaderText = "title_id";
+            this.titleidDataGridViewTextBoxColumn.Name = "titleidDataGridViewTextBoxColumn";
+            // 
+            // salesBindingSource
+            // 
+            this.salesBindingSource.DataMember = "sales";
+            this.salesBindingSource.DataSource = this.saleData;
+            // 
+            // saleData
+            // 
+            this.saleData.DataSetName = "SaleData";
+            this.saleData.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // salesTableAdapter
+            // 
+            this.salesTableAdapter.ClearBeforeFill = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(354, 32);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(176, 19);
+            this.txtSearch.TabIndex = 5;
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtsearch_keydown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(542, 399);
+            this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.cbSearch);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cbColumn);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saleData)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,8 +185,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbColumn;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cbSearch;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private SaleData saleData;
+        private System.Windows.Forms.BindingSource salesBindingSource;
+        private SaleDataTableAdapters.salesTableAdapter salesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn storidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ordnumDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orddateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qtyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn paytermsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox txtSearch;
     }
 }
 
--- 017_Search_DataGridView_Using_ComboBox_And_TxtBox/017_Search_DataGridView_Using_ComboBox_And_TxtBox/Form1.cs
+++ 017_Search_DataGridView_Using_ComboBox_And_TxtBox/017_Search_DataGridView_Using_ComboBox_And_TxtBox/Form1.cs
@@ -16,5 +16,28 @@ namespace _017_Search_DataGridView_Using_ComboBox_And_TxtBox
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            // TODO: This line of code loads data into the 'saleData.sales' table. You can move, or remove it, as needed.
+            this.salesTableAdapter.Fill(this.saleData.sales);
+            // TODO: This line of code loads data into the 'kagayaDataSet.CUSTOMERS' table. You can move, or remove it, as needed.
+            cbColumn.SelectedIndex = 0;
+
+        }
+
+        private void txtsearch_keydown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (string.IsNullOrEmpty(txtSearch.Text))
+                {
+                    salesBindingSource.Filter = string.Empty;
+                }
+                else
+                    salesBindingSource.Filter = string.Format("{0} = {1}", cbColumn.Text, txtSearch.Text);
+
+            }
+        }
     }
 }
