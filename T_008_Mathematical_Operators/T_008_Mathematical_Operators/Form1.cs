72860adcf3f3d964109d4bb8c431c0721ce0cebf
john <jm_sasuke25@yahoo.com>
2018-01-13 02:55:26 +0900

    Done

--- T_008_Mathematical_Operators/T_008_Mathematical_Operators/Form1.cs
+++ T_008_Mathematical_Operators/T_008_Mathematical_Operators/Form1.cs
@@ -16,5 +16,42 @@ namespace T_008_Mathematical_Operators
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // easy mathematical operator 
+            // + - * / % 
+            //
+            int a = 5;
+            int b = 3;
+            // in the method u need to use () to seperated to much () in this 1line
+            MessageBox.Show("a + b = " + (a + b).ToString());
+            MessageBox.Show("a - b = " + (a - b).ToString());
+            MessageBox.Show("a * b = " + (a * b).ToString());
+            MessageBox.Show("a / b = " + (a / b).ToString());
+            MessageBox.Show("a % b = " + (a % b).ToString());
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // shorthand operator
+            // ++ -- += -= *= /=
+            // self explanatory
+            int a = 5;
+            int b = 3;
+            a += 3;
+            MessageBox.Show(a.ToString());
+            // using -- prefix posfix ++
+            int c = 10;
+            MessageBox.Show("--C =" +(--c).ToString());
+             c = 10;
+            MessageBox.Show("C-- =" +(c--).ToString());
+            // print again
+            MessageBox.Show("C =" + (c).ToString());
+
+
+
+        }
     }
 }
