cc040cae5710cdd1c446e15c44343ec6f1325205
john <jm_sasuke25@yahoo.com>
2018-01-13 02:35:16 +0900

    Done

---
+++ T_008_Mathematical_Operators/T_008_Mathematical_Operators/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_008_Mathematical_Operators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
