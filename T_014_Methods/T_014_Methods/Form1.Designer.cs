d4cc141291dbdd7b4cabd08db03c0f6a0868e541
john <jm_sasuke25@yahoo.com>
2018-01-13 05:19:53 +0900

    Done

--- T_014_Methods/T_014_Methods/Form1.Designer.cs
+++ T_014_Methods/T_014_Methods/Form1.Designer.cs
@@ -29,22 +29,35 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(97, 104);
+            this.button1.Location = new System.Drawing.Point(38, 66);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "MYButton";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Methods";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- T_014_Methods/T_014_Methods/Form1.cs
+++ T_014_Methods/T_014_Methods/Form1.cs
@@ -16,5 +16,26 @@ namespace T_014_Methods
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Message("kagaya");
+        }
+        // methods you can use it over and over 
+        // void no return type
+        private void Message(string name)
+        {
+            MessageBox.Show("Hello Methods " + name);
+        }
+
+        private  int calculate(int x , int y)
+        {   int sum = x + y;
+            return sum;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {   // you can call the methods over and over with deffirent arg
+            Message("button2");
+            MessageBox.Show(calculate(1, 2).ToString());
+        }
     }
 }
