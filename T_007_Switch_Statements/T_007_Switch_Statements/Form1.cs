41ced1d322142cf6c98237b0f3490f1a547bb279
john <jm_sasuke25@yahoo.com>
2018-01-13 02:33:14 +0900

    Done

--- T_007_Switch_Statements/T_007_Switch_Statements/Form1.cs
+++ T_007_Switch_Statements/T_007_Switch_Statements/Form1.cs
@@ -16,5 +16,26 @@ namespace T_007_Switch_Statements
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // using switch very usefull
+           // it can avoid using to much if make more simple
+           // all code inside in switch block easy to maintain the code
+           // you can use in deffirent type like integer 
+           switch(textBox1.Text)
+            {
+                case "Kagaya":
+                    MessageBox.Show("Hello kagaya");
+                    break;
+                case "John":
+                    MessageBox.Show("Heeloo john");
+                    break;
+               
+                default :
+                    MessageBox.Show("Your name is'nt imported");
+                    break;
+            }
+        }
     }
 }
