063d64536c392c931a4f39bfd13d99ac376311de
john <jm_sasuke25@yahoo.com>
2018-01-13 03:20:42 +0900

    Done

--- T_009_Arrays/T_009_Arrays/Form1.Designer.cs
+++ T_009_Arrays/T_009_Arrays/Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(107, 102);
+            this.button1.Location = new System.Drawing.Point(50, 111);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,13 +42,25 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Array";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -56,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- T_009_Arrays/T_009_Arrays/Form1.cs
+++ T_009_Arrays/T_009_Arrays/Form1.cs
@@ -18,8 +18,20 @@ namespace T_009_Arrays
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        {   // Array 
+            string[] Names = { "Adam", "Bob" };
+            // foreach you can access all in the array
+            foreach (string s in Names) 
+            {
+                MessageBox.Show(s);
+            }
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {   // you can access to the aray[0] like this index 
+            // Array start 0 index
+            int[] numbers = { 555, 777, 444, 22 };
+            MessageBox.Show(numbers[0].ToString());
         }
     }
 }
