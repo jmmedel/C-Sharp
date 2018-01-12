f1c84d98c3b4cbec0af976a946c87677a803aeed
john <jm_sasuke25@yahoo.com>
2018-01-13 00:03:26 +0900

    Done

--- T_002_Showing_MessageBoxes/T_002_Showing_MessageBoxes/Form1.cs
+++ T_002_Showing_MessageBoxes/T_002_Showing_MessageBoxes/Form1.cs
@@ -16,5 +16,15 @@ namespace T_002_Showing_MessageBoxes
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // this is how to Messagebox
+            MessageBox.Show("This is text for messageBox ","Error");
+        }
+        // you can see all the Event for button
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("You are Hover your mouse over the button");
+        }
     }
 }
