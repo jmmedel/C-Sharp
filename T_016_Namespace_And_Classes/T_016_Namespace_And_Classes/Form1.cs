6ffc3a97e8becf50f1d686a1dd0c1fa44d6f611b
john <jm_sasuke25@yahoo.com>
2018-01-13 13:08:47 +0900

    Done

---
+++ T_016_Namespace_And_Classes/T_016_Namespace_And_Classes/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyClassspace;
+
+
+namespace T_016_Namespace_And_Classes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // pass the contrcutor arg 
+            MyClass class1 = new MyClass("Kagaya");
+            // calling the class.Name getter 
+            MessageBox.Show(class1.Name);
+        }
+    }
+}
---
+++ T_016_Namespace_And_Classes/T_016_Namespace_And_Classes/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// namespace
+namespace MyClassspace
+{   
+    // class
+    class MyClass
+    {   // contructor ctor space to shortcut
+        // getter and setter
+        public string Name { get; set; }
+        public MyClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
