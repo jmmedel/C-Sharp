e5e6f78c1609dedb88fdc3fed5dd2f7368a0a093
john <jm_sasuke25@yahoo.com>
2018-01-14 12:30:12 +0900

    Done

---
+++ T_021_Inheritance_And_Overriding/T_021_Inheritance_And_Overriding/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_021_Inheritance_And_Overriding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MysecondClass msc = new MysecondClass();
+            //msc.ShowMessage("Message");
+            msc.overideshowmessage("Message");
+
+            
+        }
+    }
+}
---
+++ T_021_Inheritance_And_Overriding/T_021_Inheritance_And_Overriding/MyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace T_021_Inheritance_And_Overriding
+{    // base class
+    class MyClass
+    {   // public can only be inherited
+        // protected only can access in base and drive class
+        // cant accces out side of the 2
+        protected string Name = "Kagaya";
+        protected int Age = 10;
+        public void ShowMessage(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+        public virtual void  overideshowmessage(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+    }
+    // drive class 
+    class MysecondClass : MyClass
+    {
+        public string Haircolor = "Brown";
+        public void Messageboxspecial()
+        {    //access to the base class 
+             //MessageBox.Show(base.Age.ToString());
+           
+        }
+        // overide the methods using new
+        public new void ShowMessage(string msg)
+        {
+            MessageBox.Show(msg,"MY Tiitle");
+        }
+        // override using virtual and override keywords
+        public override void overideshowmessage(string msg)
+        {
+            base.overideshowmessage(msg);
+        }
+    }
+
+}
