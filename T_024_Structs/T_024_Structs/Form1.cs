a4976960a02d824bef5c21073cffc98c49370bc9
john <jm_sasuke25@yahoo.com>
2018-01-16 23:11:03 +0900

    Done

---
+++ T_024_Structs/T_024_Structs/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_024_Structs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Client client1 = new Client();
+            client1.Name = "Kagaya";
+            client1.Age = 24;
+            Client client2 = new Client();
+            client2.Name = "Joe";
+            client2.Age = 12;
+
+        }
+    }
+}
---
+++ T_024_Structs/T_024_Structs/MyClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_024_Structs
+{
+    // struct
+    struct Client 
+    {
+        public Client(string name)
+        {
+            Name = name;
+            Age = 0;
+        }
+        public string Name;
+        public int Age;
+        public void ClearClientInfo()
+        {
+            Name = "";
+            Age = 0;
+
+        }
+    }
+
+    
+
+}
