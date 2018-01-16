0e02c057d00ffb2c88a1bea4009191896181d4ea
john <jm_sasuke25@yahoo.com>
2018-01-17 04:16:27 +0900

    Done

---
+++ T_028_Events/T_028_Events/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_028_Events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyClass mc = new MyClass();
+            mc.OnPropertyChanged += Mc_OnPropertyChanged;
+            mc.Name = "Kagaya";
+        }
+
+        private void Mc_OnPropertyChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test the property has changed");
+        }
+    }
+}
---
+++ T_028_Events/T_028_Events/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_028_Events
+{
+    class MyClass
+    {   // making your own even
+        public event EventHandler OnPropertyChanged;
+        public string name = "";
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged(this, new EventArgs());
+
+            }
+        }
+    }
+}
