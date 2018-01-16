3660e271b5d39fb58a0a0d94b164b235342d8373
john <jm_sasuke25@yahoo.com>
2018-01-17 03:49:31 +0900

    Done

---
+++ T_027_Delegates/T_027_Delegates/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_027_Delegates
+{
+    class MyClass
+
+    {   // delagate
+        delegate void Mydelegate(string mystring);
+        public void ShowThoseMessage()
+        {   // adding to the delegate
+            Mydelegate md = new Mydelegate(ShowMessage);
+            md += ShowAnotherMessage;
+            md("kagaya");
+            
+        }
+
+        void ShowMessage(string msg)
+        {
+            System.Windows.Forms.MessageBox.Show(msg);
+        }
+
+        void ShowAnotherMessage(string a)
+        {
+            System.Windows.Forms.MessageBox.Show(a,"Test");
+        }
+    }
+
+}
