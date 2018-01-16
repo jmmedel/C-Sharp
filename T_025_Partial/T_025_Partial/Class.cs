11f0c79af9ac90dad2a3522fcf5667e78b53888f
john <jm_sasuke25@yahoo.com>
2018-01-17 00:09:07 +0900

    Done

---
+++ T_025_Partial/T_025_Partial/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_025_Partial
+{   // combine  two class with the same filename
+    partial class MyClass
+    {
+        public string HairColor = "Brown";
+        public bool Glasses = false;
+        // partial 
+        partial void Message(string msg)
+        {
+            System.Windows.Forms.MessageBox.Show(msg);
+        }
+        public void ShowMsg(string message)
+        {
+            Message(message);
+        }
+    }
+
+
+}
---
+++ T_025_Partial/T_025_Partial/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_025_Partial
+{ // combine  two class with the same filename
+    partial class MyClass
+    {
+        public string Name = "Kagaya";
+        public  int Age = 24;
+        // partial methods
+        partial void Message(string msg);
+
+    }
+
+    
+}
