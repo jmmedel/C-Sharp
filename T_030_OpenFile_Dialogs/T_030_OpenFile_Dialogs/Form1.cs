075c35aa75709392c5191b975f927e8065affaf8
john <jm_sasuke25@yahoo.com>
2018-01-18 06:01:16 +0900

    Done

---
+++ T_030_OpenFile_Dialogs/T_030_OpenFile_Dialogs/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_030_OpenFile_Dialogs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            // give the filter and set only to png
+            ofd.Filter = "PNG image | *.png";
+            ofd.Title = "Open Image";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {   
+                // get the filename of the file
+                MessageBox.Show(ofd.SafeFileName);
+            }
+
+        }
+    }
+}
