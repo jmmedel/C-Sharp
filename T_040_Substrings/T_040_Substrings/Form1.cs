ec47da89223699b537afdaa04f42a6027edce594
john <jm_sasuke25@yahoo.com>
2018-01-20 14:35:46 +0900

    Done

---
+++ T_040_Substrings/T_040_Substrings/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_040_Substrings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // using substring 
+            string Name = "John Medel";
+            // start with 0 index and get 4 letter
+            string LastName = Name.Substring(0,4);
+            MessageBox.Show(LastName.ToString());
+        }
+    }
+}
