10d841d808d946eed1b4d28710308a37702fe1a2
john <jm_sasuke25@yahoo.com>
2018-01-20 14:29:00 +0900

    done

---
+++ T_039_is_as_And_Casting/T_039_is_as_And_Casting/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_039_is_as_And_Casting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // casting
+            // obj is univerlsal object type i can be any type
+            object myobj = "Kagaya";
+            Control myControl = button1;
+            if (myobj is string) MessageBox.Show("myobj is a string True");
+            if (myControl is Button)
+            {
+                //Button mybutton = (Button)myControl; // using as a casting
+                Button mybutton = myControl as Button; // simple way to cast
+                MessageBox.Show(mybutton.Text);
+            }
+        }
+
+    }
+}
---
+++ T_039_is_as_And_Casting/T_039_is_as_And_Casting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_039_is_as_And_Casting
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
