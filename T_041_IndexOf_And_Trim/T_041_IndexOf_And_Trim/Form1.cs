7dd3304c71e84b79f1ea70190e133343e6fb33ab
john <jm_sasuke25@yahoo.com>
2018-01-20 14:36:49 +0900

    Done

---
+++ T_041_IndexOf_And_Trim/T_041_IndexOf_And_Trim/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_041_IndexOf_And_Trim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
