b5dd82a8fddacb8dd9b99ec4c321c96eb1026cb3
john <jm_sasuke25@yahoo.com>
2018-01-20 12:13:38 +0900

    Done

--- T_034_BinaryReader/T_034_BinaryReader/Form1.cs
+++ T_034_BinaryReader/T_034_BinaryReader/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace T_034_BinaryReader
 {
     public partial class Form1 : Form
@@ -15,6 +15,32 @@ namespace T_034_BinaryReader
         public Form1()
         {
             InitializeComponent();
+        }
+        string path;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = true;
+                path = ofd.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BinaryReader br = new BinaryReader(File.OpenRead(path));
+            //br.BaseStream.Position = 0x10;
+
+            //textBox1.Text = br.ReadChar().ToString();
+            foreach (var mychar in br.ReadChars(200))
+            {
+                textBox1.Text += mychar;
+            }
+            
+            //close  all the time
+            br.Dispose();
+
         }
     }
 }
