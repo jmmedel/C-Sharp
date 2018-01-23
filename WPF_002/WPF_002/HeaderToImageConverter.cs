b462115c9840056ef22bf5706a8dfc50efdefada
john <jm_sasuke25@yahoo.com>
2018-01-24 00:32:12 +0900

    Fix the Image folder name mistake

--- WPF_002/WPF_002/HeaderToImageConverter.cs
+++ WPF_002/WPF_002/HeaderToImageConverter.cs
@@ -27,16 +27,16 @@ namespace WpfTreeView
             var name = MainWindow.GetFileFolderName(path);
 
             // By default, we presume an image
-            var image = "Images/file.png";
+            var image = "Image/file.png";
 
             // If the name is blank, we presume it's a drive as we cannot have a blank file or folder name
             if (string.IsNullOrEmpty(name))
-                image = "Images/drive.png";
+                image = "Image/drive.png";
             else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
-                image = "Images/folder-closed.png";
+                image = "Image/folder-closed.png";
 
             //return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
-            return new BitmapImage(new Uri($"pack://application:,,,/Image/drive.png"));
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
