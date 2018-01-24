ffb1bef9f375cdf8179c3309edc38c2abe63fb18
john <jm_sasuke25@yahoo.com>
2018-01-24 22:33:21 +0900

    Done

---
+++ T_045_Generating_Random_Number/T_045_Generating_Random_Number/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_045_Generating_Random_Number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // using ramdom 
+            Random r = new Random();
+            label1.Text = r.Next(1,10).ToString();
+            label2.Text = r.Next(1, 10).ToString();
+            label3.Text = r.Next(1, 10).ToString();
+            label4.Text = r.Next(1, 10).ToString();
+            label5.Text = r.Next(1, 10).ToString();
+            label6.Text = r.Next(1, 10).ToString();
+
+        }
+    }
+}
