ad2188f0089a7a169bbf3592a77e8cf026dd3c30
john <jm_sasuke25@yahoo.com>
2018-01-25 01:06:37 +0900

    Done

---
+++ T_048_Directory_Class/T_048_Directory_Class/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_048_Directory_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // testing any more control
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
