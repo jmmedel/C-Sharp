828f137288c0d28f272e7ad561341fc1e561224e
john <jm_sasuke25@yahoo.com>
2018-01-26 01:58:45 +0900

    Done

--- T_048_Directory_Class/T_048_Directory_Class/Form1.Designer.cs
+++ T_048_Directory_Class/T_048_Directory_Class/Form1.Designer.cs
@@ -30,31 +30,49 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDriveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem});
+            this.openToolStripMenuItem,
+            this.openDirectoryToolStripMenuItem,
+            this.openDriveToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(319, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.openToolStripMenuItem.Text = "Open File Name";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openDirectoryToolStripMenuItem
+            // 
+            this.openDirectoryToolStripMenuItem.Name = "openDirectoryToolStripMenuItem";
+            this.openDirectoryToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.openDirectoryToolStripMenuItem.Text = "OpenDirectory";
+            this.openDirectoryToolStripMenuItem.Click += new System.EventHandler(this.openDirectoryToolStripMenuItem_Click);
+            // 
+            // openDriveToolStripMenuItem
+            // 
+            this.openDriveToolStripMenuItem.Name = "openDriveToolStripMenuItem";
+            this.openDriveToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.openDriveToolStripMenuItem.Text = "OpenDrive";
+            this.openDriveToolStripMenuItem.Click += new System.EventHandler(this.openDriveToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 166);
+            this.ClientSize = new System.Drawing.Size(319, 166);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -70,6 +88,8 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDriveToolStripMenuItem;
     }
 }
 
--- T_048_Directory_Class/T_048_Directory_Class/Form1.cs
+++ T_048_Directory_Class/T_048_Directory_Class/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace T_048_Directory_Class
 {
     public partial class Form1 : Form
@@ -19,7 +19,38 @@ namespace T_048_Directory_Class
         // testing any more control
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            // using Directory class
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {   // get the file names
+                 string [] filesPath = Directory.GetFiles(fbd.SelectedPath);
+                foreach (string item in filesPath)
+                {
+                    MessageBox.Show(item);
+                }
+            }
+        }
 
+        private void openDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string[] FileDirectory = Directory.GetDirectories(fbd.SelectedPath);
+                foreach (string item in FileDirectory)
+                {
+                    MessageBox.Show(item);
+                }
+            }
+        }
+
+        private void openDriveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] drivers = Directory.GetLogicalDrives();
+            foreach(string s in drivers)
+            {
+                MessageBox.Show(s);
+            }
         }
     }
 }
