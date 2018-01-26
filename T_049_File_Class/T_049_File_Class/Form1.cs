e131bfa9de698db19b6c9f4e9b42566b3347af55
john <jm_sasuke25@yahoo.com>
2018-01-27 04:26:25 +0900

    Done

---
+++ T_049_File_Class/T_049_File_Class/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace T_049_File_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // open fileDialog
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                MessageBox.Show(File.Exists(ofd.FileName).ToString());
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // delete file
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                File.Delete(ofd.FileName);
+        }
+    }
+}
