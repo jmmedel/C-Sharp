22e2891b23452579ed14a1332e1c5a04e9393c69
john <jm_sasuke25@yahoo.com>
2018-01-30 22:01:11 +0900

    Done

--- T_051_Process_Class/T_051_Process_Class/Form1.cs
+++ T_051_Process_Class/T_051_Process_Class/Form1.cs
@@ -42,5 +42,32 @@ namespace T_051_Process_Class
         {   // kill the process() 
             Process.GetCurrentProcess().Kill();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {   // get all the process name
+            foreach (Process p in Process.GetProcesses())
+            {
+                // MessageBox.Show(p.ProcessName);
+                MessageBox.Show(p.Responding.ToString());
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {   // kill all the program not responding 
+            foreach (Process p in Process.GetProcesses())
+            {
+                if(!p.Responding)
+                {
+                    p.Kill();
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            foreach(Process p in Process.GetProcessesByName("Skype"))
+                MessageBox.Show(p.ProcessName);
+        }
     }
 }
