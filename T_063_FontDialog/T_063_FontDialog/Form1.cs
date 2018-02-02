0488c8b75c3f4f26ea3b314d0b5ec186cb1b612e
john <jm_sasuke25@yahoo.com>
2018-02-03 01:15:47 +0900

    Done

---
+++ T_063_FontDialog/T_063_FontDialog/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_063_FontDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            fd.MinSize = 10;
+            fd.MaxSize = 20;
+            fd.ShowColor = true;
+            fd.ShowHelp = true;
+            fd.HelpRequest += new EventHandler(fd_helprequest)
+            if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Font = fd.Font;
+                textBox1.ForeColor = fd.Color;
+            }
+        }
+
+        private void fd_helprequest(object sender, EventArgs e)
+        {
+            MessageBox.Show("Choose a font for the textbox");
+        }
+    }
+}
