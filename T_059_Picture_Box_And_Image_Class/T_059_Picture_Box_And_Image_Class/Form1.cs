7c352b5f4971e967c6b53a17b85b218bd1861bac
john <jm_sasuke25@yahoo.com>
2018-02-03 00:01:32 +0900

    Done

---
+++ T_059_Picture_Box_And_Image_Class/T_059_Picture_Box_And_Image_Class/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_059_Picture_Box_And_Image_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // one file dialog
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {   
+                // you can put url for location to 
+                pictureBox1.ImageLocation = ofd.FileName;
+
+            }
+
+        }
+    }
+}
