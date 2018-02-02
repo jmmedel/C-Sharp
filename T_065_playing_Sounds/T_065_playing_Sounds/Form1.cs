9d2972ee88cd8042981a6893353e516ae0b596d1
john <jm_sasuke25@yahoo.com>
2018-02-03 02:26:15 +0900

    Done

---
+++ T_065_playing_Sounds/T_065_playing_Sounds/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace T_065_playing_Sounds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                SoundPlayer s = new SoundPlayer(ofd.FileName);
+                // play the media
+                //s.Play();
+                
+                s.PlayLooping();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   // the beep sound 
+            SystemSounds.Asterisk.Play();
+        }
+    }
+}
