7f06ecb8bf61ece7dde305adf565ff3a5380ddf8
john <jm_sasuke25@yahoo.com>
2018-02-04 12:45:45 +0900

    Done

---
+++ T_071_ListView_Control/T_071_ListView_Control/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_071_ListView_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // make a check if empty or null
+            ListViewItem item1;
+            
+                item1 = new ListViewItem(textBox1.Text);
+                item1.SubItems.Add(textBox3.Text);
+                item1.SubItems.Add(textBox2.Text);
+                listView1.Items.Add(item1);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+          
+           
+               
+
+        }
+    }
+}
