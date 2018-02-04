039702206d75addb60cad92d82f106039925fdba
john <jm_sasuke25@yahoo.com>
2018-02-04 10:22:34 +0900

    Done

---
+++ T_070_ProgressBar_Control/T_070_ProgressBar_Control/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_070_ProgressBar_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // use it in block style progress bar
+            // use performstep to not get and error
+            //progressBar1.PerformStep();
+
+            // next is marquee
+            // if you dont know when is end 
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 100;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {    // block style progress bar
+            //progressBar1.Value = 0;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+
+        }
+    }
+}
