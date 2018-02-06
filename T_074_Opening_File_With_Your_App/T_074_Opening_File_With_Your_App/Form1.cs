865308c55488912abd98be5f0c8101faa5b9045d
john <jm_sasuke25@yahoo.com>
2018-02-07 01:51:59 +0900

    Done

---
+++ T_074_Opening_File_With_Your_App/T_074_Opening_File_With_Your_App/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_074_Opening_File_With_Your_App
+{
+    public partial class Form1 : Form
+    {   
+        
+        public Form1(string path)
+        {
+            MessageBox.Show(path);
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ T_074_Opening_File_With_Your_App/T_074_Opening_File_With_Your_App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_074_Opening_File_With_Your_App
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] files)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            foreach (string item in files)
+            {
+                Application.Run(new Form1(item));
+            }
+           
+        }
+    }
+}
