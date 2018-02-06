1cb8ec0cc63aa33e5966245668721afd17d82f28
john <jm_sasuke25@yahoo.com>
2018-02-07 02:56:58 +0900

    Done

--- T_076_TreeView_Control/T_076_TreeView_Control/Form1.cs
+++ T_076_TreeView_Control/T_076_TreeView_Control/Form1.cs
@@ -29,5 +29,38 @@ namespace T_076_TreeView_Control
             //add node inside the node
             treeView1.Nodes[0].Nodes[0].Nodes.Add("Address");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   // remove selected node
+            // handle if its null 
+            if(treeView1.SelectedNode == null)
+            {
+                return ;
+            }
+            treeView1.SelectedNode.Remove();
+            // i dont like to use checkedbox to delete to the treeview
+            // dont use it
+           // RemoveSelectedNode(treeView1.Nodes);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+        }
+        List<TreeNode> tnlist = new List<TreeNode>();
+
+        void RemoveSelectedNode(TreeNodeCollection tnc)
+        {
+            foreach  ( TreeNode item  in tnc)
+            {
+                if (item.Checked) tnlist.Add(item);
+                else if (item.Nodes.Count != 0) RemoveSelectedNode(item.Nodes);
+            }
+            foreach (TreeNode item in tnlist)
+            {
+                treeView1.Nodes.Remove(item);
+            }
+        }
+
     }
 }
