4d2a51d6dc2d78139f28920db9e73d776a04cbf0
john <jm_sasuke25@yahoo.com>
2018-02-07 01:33:53 +0900

    Done

---
+++ T_073_NotifyIcon_Control/T_073_NotifyIcon_Control/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_073_NotifyIcon_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // use it hide your app notifiy 
+            this.Hide();
+            notifyIcon1.ShowBalloonTip(1000,"My tiitle","Your app is still running",ToolTipIcon.Info);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+    }
+}
