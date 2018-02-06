47fae77dffe885ed9fde084cccc0d5d5095cb7b4
john <jm_sasuke25@yahoo.com>
2018-02-07 02:13:15 +0900

    Done

---
+++ T_075_Settings/T_075_Settings/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_075_Settings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {   // this is where you need to put the setting
+            //because this is where form create first call the constructor
+            // think it like main where the program start fist
+            InitializeComponent();
+            textBox1.Text = T_075_Settings.Properties.Settings.Default.Name;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            // click project 
+            // project properties
+            // go to setting
+            // add string name
+            T_075_Settings.Properties.Settings.Default.Name = textBox1.Text;
+            T_075_Settings.Properties.Settings.Default.Save();
+        }
+    }
+}
