c0b13f5435e0a1eb57bcbbb62f029b3f3591d366
john <jm_sasuke25@yahoo.com>
2018-02-07 20:08:54 +0900

    Test_Bot_View

---
+++ T_080_WebBrowser_Control/T_080_WebBrowser_Control/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace T_080_WebBrowser_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser3 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser4 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser5 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser6 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser7 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser8 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser9 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser10 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser11 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser12 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser13 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser14 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser15 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(379, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 72);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Go_Back";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(106, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(197, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Go_Forwards";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(288, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Go_home";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(106, 72);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser2.TabIndex = 7;
+            // 
+            // webBrowser3
+            // 
+            this.webBrowser3.Location = new System.Drawing.Point(200, 72);
+            this.webBrowser3.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser3.Name = "webBrowser3";
+            this.webBrowser3.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser3.TabIndex = 8;
+            // 
+            // webBrowser4
+            // 
+            this.webBrowser4.Location = new System.Drawing.Point(294, 72);
+            this.webBrowser4.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser4.Name = "webBrowser4";
+            this.webBrowser4.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser4.TabIndex = 9;
+            // 
+            // webBrowser5
+            // 
+            this.webBrowser5.Location = new System.Drawing.Point(388, 72);
+            this.webBrowser5.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser5.Name = "webBrowser5";
+            this.webBrowser5.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser5.TabIndex = 10;
+            // 
+            // webBrowser6
+            // 
+            this.webBrowser6.Location = new System.Drawing.Point(12, 180);
+            this.webBrowser6.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser6.Name = "webBrowser6";
+            this.webBrowser6.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser6.TabIndex = 11;
+            // 
+            // webBrowser7
+            // 
+            this.webBrowser7.Location = new System.Drawing.Point(106, 180);
+            this.webBrowser7.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser7.Name = "webBrowser7";
+            this.webBrowser7.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser7.TabIndex = 12;
+            // 
+            // webBrowser8
+            // 
+            this.webBrowser8.Location = new System.Drawing.Point(197, 180);
+            this.webBrowser8.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser8.Name = "webBrowser8";
+            this.webBrowser8.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser8.TabIndex = 13;
+            // 
+            // webBrowser9
+            // 
+            this.webBrowser9.Location = new System.Drawing.Point(294, 180);
+            this.webBrowser9.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser9.Name = "webBrowser9";
+            this.webBrowser9.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser9.TabIndex = 14;
+            // 
+            // webBrowser10
+            // 
+            this.webBrowser10.Location = new System.Drawing.Point(388, 180);
+            this.webBrowser10.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser10.Name = "webBrowser10";
+            this.webBrowser10.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser10.TabIndex = 15;
+            // 
+            // webBrowser11
+            // 
+            this.webBrowser11.Location = new System.Drawing.Point(13, 288);
+            this.webBrowser11.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser11.Name = "webBrowser11";
+            this.webBrowser11.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser11.TabIndex = 16;
+            // 
+            // webBrowser12
+            // 
+            this.webBrowser12.Location = new System.Drawing.Point(107, 288);
+            this.webBrowser12.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser12.Name = "webBrowser12";
+            this.webBrowser12.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser12.TabIndex = 17;
+            // 
+            // webBrowser13
+            // 
+            this.webBrowser13.Location = new System.Drawing.Point(201, 288);
+            this.webBrowser13.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser13.Name = "webBrowser13";
+            this.webBrowser13.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser13.TabIndex = 18;
+            // 
+            // webBrowser14
+            // 
+            this.webBrowser14.Location = new System.Drawing.Point(295, 288);
+            this.webBrowser14.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser14.Name = "webBrowser14";
+            this.webBrowser14.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser14.TabIndex = 19;
+            // 
+            // webBrowser15
+            // 
+            this.webBrowser15.Location = new System.Drawing.Point(375, 288);
+            this.webBrowser15.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser15.Name = "webBrowser15";
+            this.webBrowser15.Size = new System.Drawing.Size(88, 102);
+            this.webBrowser15.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 418);
+            this.Controls.Add(this.webBrowser15);
+            this.Controls.Add(this.webBrowser14);
+            this.Controls.Add(this.webBrowser13);
+            this.Controls.Add(this.webBrowser12);
+            this.Controls.Add(this.webBrowser11);
+            this.Controls.Add(this.webBrowser10);
+            this.Controls.Add(this.webBrowser9);
+            this.Controls.Add(this.webBrowser8);
+            this.Controls.Add(this.webBrowser7);
+            this.Controls.Add(this.webBrowser6);
+            this.Controls.Add(this.webBrowser5);
+            this.Controls.Add(this.webBrowser4);
+            this.Controls.Add(this.webBrowser3);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.WebBrowser webBrowser3;
+        private System.Windows.Forms.WebBrowser webBrowser4;
+        private System.Windows.Forms.WebBrowser webBrowser5;
+        private System.Windows.Forms.WebBrowser webBrowser6;
+        private System.Windows.Forms.WebBrowser webBrowser7;
+        private System.Windows.Forms.WebBrowser webBrowser8;
+        private System.Windows.Forms.WebBrowser webBrowser9;
+        private System.Windows.Forms.WebBrowser webBrowser10;
+        private System.Windows.Forms.WebBrowser webBrowser11;
+        private System.Windows.Forms.WebBrowser webBrowser12;
+        private System.Windows.Forms.WebBrowser webBrowser13;
+        private System.Windows.Forms.WebBrowser webBrowser14;
+        private System.Windows.Forms.WebBrowser webBrowser15;
+    }
+}
+
---
+++ T_080_WebBrowser_Control/T_080_WebBrowser_Control/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_080_WebBrowser_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w";
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser2.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser3.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser4.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser5.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser6.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser7.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser8.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser9.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser10.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser11.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser12.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+
+            webBrowser13.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser14.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+            webBrowser15.Navigate("https://www.youtube.com/watch?v=sTiA4Xk6Tzs&list=PLaJv8CC2dxuSZ6bgcZS-IqnZTJGKKmz3w");
+           
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox1.Text = webBrowser1.Url.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+            webBrowser2.Refresh();
+            webBrowser3.Refresh();
+            webBrowser4.Refresh();
+            webBrowser5.Refresh();
+            webBrowser6.Refresh();
+            webBrowser7.Refresh();
+            webBrowser8.Refresh();
+            webBrowser9.Refresh();
+            webBrowser10.Refresh();
+
+            webBrowser11.Refresh();
+            webBrowser12.Refresh();
+            webBrowser13.Refresh();
+            webBrowser14.Refresh();
+            webBrowser15.Refresh();
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+    }
+}
