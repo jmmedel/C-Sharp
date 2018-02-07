cf297ba7b77ffa8cb9e3dad99c654c4c363f72d7
john <jm_sasuke25@yahoo.com>
2018-02-07 19:30:29 +0900

    Done

--- T_079_Accessing_All_Control/T_079_Accessing_All_Control/Form1.cs
+++ T_079_Accessing_All_Control/T_079_Accessing_All_Control/Form1.cs
@@ -16,5 +16,32 @@ namespace T_079_Accessing_All_Control
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AccessAll(this.Controls);
+        }
+        // AccessAll control 
+        void AccessAll(Control.ControlCollection cc)
+        {
+            foreach (Control item in cc)
+            {   // this enabled all 
+                //item.Enabled = false;
+                if (item is Button)
+                {
+                    Button b = item as Button;
+                    b.Click +=  new EventHandler(B_Click);
+                }
+                if(item.HasChildren)
+                {
+                    AccessAll(item.Controls);
+                }
+            }
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("you  click a button");
+        }
     }
 }
