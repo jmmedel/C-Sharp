161c322a9514c2bee30a6ee284d1277459214e62
john <jm_sasuke25@yahoo.com>
2018-02-14 17:12:14 +0900

    Done

---
+++ T_083_Reading_XML/T_083_Reading_XML/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+namespace T_083_Reading_XML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "XML|*.xml";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {   // REad xml file 
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load(ofd.FileName);
+                // this is only see one file a times
+                //MessageBox.Show(xDoc.SelectSingleNode("People/Person/Name").InnerText);
+                foreach (XmlNode node in xDoc.SelectNodes("People/Person"))
+                {
+                    MessageBox.Show(node.SelectSingleNode("Name").InnerText);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   // url here put the url 
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("");
+            MessageBox.Show(xDoc.SelectSingleNode("People/Person/Name").InnerText);
+        }
+    }
+}
