d5618f64007d823a83203409cd59bc2ec660d953
john <jm_sasuke25@yahoo.com>
2018-02-15 00:38:32 +0900

    Done

--- T_085_Writing_New_XMl_File/T_085_Writing_New_XMl_File/Form1.cs
+++ T_085_Writing_New_XMl_File/T_085_Writing_New_XMl_File/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Xml;
 namespace T_085_Writing_New_XMl_File
 {
     public partial class Form1 : Form
@@ -15,6 +15,29 @@ namespace T_085_Writing_New_XMl_File
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // you can use openfile dialog here but im to lazy to that know 
+            string path = "C:\\Users\\kagaya\\Desktop\\Git\\T_085_Writing_New_XMl_File\\T_085_Writing_New_XMl_File\\Person.xml";
+            XmlTextWriter xwriter = new XmlTextWriter(path, Encoding.UTF8);
+            xwriter.Formatting = Formatting.Indented;
+            xwriter.WriteStartElement("People");//<People>
+            xwriter.WriteStartElement("Person");// <Person>
+            xwriter.WriteStartElement("Name"); // <Name>
+            xwriter.WriteString(textBox1.Text); //Textbox1.Text
+            xwriter.WriteEndElement();//</Name>
+            xwriter.WriteStartElement("Age");//<Age>
+            xwriter.WriteString(numericUpDown1.Value.ToString());
+            xwriter.WriteEndElement();//</Age>
+            xwriter.WriteStartElement("Email");//<Email>
+            xwriter.WriteString(textBox2.Text);
+            xwriter.WriteEndElement();//</Email>
+            xwriter.WriteEndElement();//</Person>
+            xwriter.WriteEndElement();//</People>
+            xwriter.Close();
+
+
         }
     }
 }
