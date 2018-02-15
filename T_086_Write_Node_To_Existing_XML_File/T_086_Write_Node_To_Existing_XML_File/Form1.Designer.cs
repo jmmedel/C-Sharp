e6621b4b260ea3af4cf82c20048a8663540be701
john <jm_sasuke25@yahoo.com>
2018-02-15 15:34:04 +0900

    Done

--- T_086_Write_Node_To_Existing_XML_File/T_086_Write_Node_To_Existing_XML_File/Form1.Designer.cs
+++ T_086_Write_Node_To_Existing_XML_File/T_086_Write_Node_To_Existing_XML_File/Form1.Designer.cs
@@ -35,7 +35,9 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -51,7 +53,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 81);
+            this.label1.Location = new System.Drawing.Point(15, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(36, 12);
             this.label1.TabIndex = 1;
@@ -60,7 +62,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 123);
+            this.label2.Location = new System.Drawing.Point(24, 52);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(27, 12);
             this.label2.TabIndex = 2;
@@ -69,7 +71,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 166);
+            this.label3.Location = new System.Drawing.Point(24, 86);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 12);
             this.label3.TabIndex = 3;
@@ -77,42 +79,54 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(110, 74);
+            this.textBox1.Location = new System.Drawing.Point(66, 15);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(139, 19);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(110, 158);
+            this.textBox2.Location = new System.Drawing.Point(66, 83);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(139, 19);
             this.textBox2.TabIndex = 5;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(110, 115);
+            this.numericUpDown1.Location = new System.Drawing.Point(66, 52);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(139, 19);
             this.numericUpDown1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 127);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 204);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(268, 212);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -125,6 +139,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- T_086_Write_Node_To_Existing_XML_File/T_086_Write_Node_To_Existing_XML_File/Form1.cs
+++ T_086_Write_Node_To_Existing_XML_File/T_086_Write_Node_To_Existing_XML_File/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Xml;
 namespace T_086_Write_Node_To_Existing_XML_File
 {
     public partial class Form1 : Form
@@ -19,6 +19,21 @@ namespace T_086_Write_Node_To_Existing_XML_File
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string path = "C:\\Users\\kagaya\\Desktop\\Git\\T_086_Write_Node_To_Existing_XML_File\\T_086_Write_Node_To_Existing_XML_File\\Person.xml";
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            XmlNode person = doc.CreateElement("Person");
+            XmlNode name = doc.CreateElement("Name");
+            name.InnerText = textBox1.Text;
+            person.AppendChild(name);
+            XmlNode age = doc.CreateElement("Age");
+            age.InnerText = numericUpDown1.Value.ToString();
+            person.AppendChild(age);
+            XmlNode Email = doc.CreateElement("Email");
+            Email.InnerText = textBox2.Text;
+            person.AppendChild(Email);
+            doc.DocumentElement.AppendChild(person);
+            doc.Save(path);
 
         }
     }
