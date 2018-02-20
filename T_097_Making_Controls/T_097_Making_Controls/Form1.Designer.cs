097cbc37002e06e7e60d84384bed460ee20f4e9c
john <jm_sasuke25@yahoo.com>
2018-02-20 22:43:17 +0900

    Not Done

--- T_097_Making_Controls/T_097_Making_Controls/Form1.Designer.cs
+++ T_097_Making_Controls/T_097_Making_Controls/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mybutton2 = new T_097_Making_Controls.Mybutton();
+            this.SuspendLayout();
+            // 
+            // mybutton2
+            // 
+            this.mybutton2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.mybutton2.ButtonText = "Kagaya";
+            this.mybutton2.Location = new System.Drawing.Point(52, 36);
+            this.mybutton2.Name = "mybutton2";
+            this.mybutton2.Size = new System.Drawing.Size(71, 27);
+            this.mybutton2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 130);
+            this.Controls.Add(this.mybutton2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Mybutton mybutton2;
     }
 }
 
---
+++ T_097_Making_Controls/T_097_Making_Controls/Mybutton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_097_Making_Controls
+{
+    public partial class Mybutton : UserControl
+    {
+        public Mybutton()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            SolidBrush s = new SolidBrush(Color.FromKnownColor(KnownColor.Control));
+            Graphics g = this.CreateGraphics();
+            g.FillRectangle(s, 0, 0, this.Width, this.Height);
+            s.Color = Color.FromKnownColor(KnownColor.ControlLight);
+            g.FillRectangle(s, 0, this.Height / 2, this.Width, this.Height / 2);
+            label1.Location = new Point((this.Width / 2)- (label1.Width / 2), (this.Height / 2)-(label1.Height / 2));
+        }
+
+        public string ButtonText
+        {
+            get { return label1.Text; }
+            set { label1.Text = value; }
+        }
+
+    }
+}
