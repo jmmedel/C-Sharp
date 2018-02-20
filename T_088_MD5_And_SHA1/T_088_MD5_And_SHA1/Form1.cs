020fabb53f4e1b04e09e4fbb36c44373e614f2e3
john <jm_sasuke25@yahoo.com>
2018-02-20 18:38:11 +0900

    Done

--- T_088_MD5_And_SHA1/T_088_MD5_And_SHA1/Form1.cs
+++ T_088_MD5_And_SHA1/T_088_MD5_And_SHA1/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Security.Cryptography;
 namespace T_088_MD5_And_SHA1
 {
     public partial class Form1 : Form
@@ -15,6 +15,18 @@ namespace T_088_MD5_And_SHA1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            Encoding utf = new UTF8Encoding();
+            MessageBox.Show(BitConverter.ToString(md5.ComputeHash(utf.GetBytes(textBox1.Text))));
+
+            SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            MessageBox.Show(BitConverter.ToString(sha1.ComputeHash(utf.GetBytes(textBox1.Text))));
+
         }
     }
 }
