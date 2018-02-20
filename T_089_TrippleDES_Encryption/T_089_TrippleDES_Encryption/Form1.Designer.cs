fe888e5e1ac56c16df3dfc3db32f77d5ca6511ff
john <jm_sasuke25@yahoo.com>
2018-02-20 19:05:51 +0900

    Done

--- T_089_TrippleDES_Encryption/T_089_TrippleDES_Encryption/Form1.Designer.cs
+++ T_089_TrippleDES_Encryption/T_089_TrippleDES_Encryption/Form1.Designer.cs
@@ -35,6 +35,11 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -96,11 +101,58 @@
             this.textBox3.Size = new System.Drawing.Size(206, 45);
             this.textBox3.TabIndex = 6;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Key:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Decrypted:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(122, 267);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 19);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(122, 298);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 19);
+            this.textBox5.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 229);
+            this.ClientSize = new System.Drawing.Size(230, 351);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -125,6 +177,11 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- T_089_TrippleDES_Encryption/T_089_TrippleDES_Encryption/Form1.cs
+++ T_089_TrippleDES_Encryption/T_089_TrippleDES_Encryption/Form1.cs
@@ -16,7 +16,7 @@ namespace T_089_TrippleDES_Encryption
         {
             InitializeComponent();
         }
-
+        byte[] encryted;
         private void button1_Click(object sender, EventArgs e)
         {
             MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
@@ -26,7 +26,30 @@ namespace T_089_TrippleDES_Encryption
             TDES.Mode = CipherMode.ECB;
             TDES.Padding = PaddingMode.PKCS7;
             ICryptoTransform trans = TDES.CreateEncryptor();
-            textBox3.Text=  BitConverter.ToString(trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text),0,utf8.GetBytes(textBox2.Text).Length));
+            encryted = trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text),0,utf8.GetBytes(textBox2.Text).Length);
+            textBox3.Text = BitConverter.ToString(encryted);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            TripleDESCryptoServiceProvider TDES = new TripleDESCryptoServiceProvider();
+            TDES.Key = md5.ComputeHash(utf8.GetBytes(textBox4.Text));
+            TDES.Mode = CipherMode.ECB;
+            TDES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform trans = TDES.CreateDecryptor();
+            try
+            {
+                textBox5.Text = utf8.GetString(trans.TransformFinalBlock(encryted, 0, encryted.Length));
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Wrong Key");
+            }
+            
 
         }
     }
