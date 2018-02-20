b92ae39c96ca6e5124d7b902a27e7af090c48c65
john <jm_sasuke25@yahoo.com>
2018-02-20 20:04:23 +0900

    Done

---
+++ T_095_LinearGradientBrush/T_095_LinearGradientBrush/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+namespace T_095_LinearGradientBrush
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            LinearGradientBrush lgb = new LinearGradientBrush(new Point(20, 20), new Point(70, 70), Color.Blue, Color.Yellow);
+            Graphics g = panel1.CreateGraphics();
+            g.FillEllipse(lgb, 20, 20, 100, 0);
+            
+        }
+    }
+}
