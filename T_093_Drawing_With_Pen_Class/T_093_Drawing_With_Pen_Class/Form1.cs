47b56f83e89bfc27a164b349725926c1932ba861
john <jm_sasuke25@yahoo.com>
2018-02-20 19:55:01 +0900

    Done

---
+++ T_093_Drawing_With_Pen_Class/T_093_Drawing_With_Pen_Class/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_093_Drawing_With_Pen_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Red,3);
+            Graphics g = panel1.CreateGraphics();
+            g.DrawRectangle(pen, 20, 20, 50, 50);
+            g.DrawEllipse(pen, 50, 50, 50, 50);
+            
+        }
+    }
+}
