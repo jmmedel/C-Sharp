d01565ff39d66da2a0d10acbf777085d087d662b
john <jm_sasuke25@yahoo.com>
2018-02-22 00:13:22 +0900

    Done

---
+++ T_098_Inheriting_From_Existing_Controls/T_098_Inheriting_From_Existing_Controls/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace T_098_Inheriting_From_Existing_Controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new T_098_Inheriting_From_Existing_Controls.UserControl1();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(106, 60);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(75, 23);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControl1 userControl11;
+    }
+}
+
---
+++ T_098_Inheriting_From_Existing_Controls/T_098_Inheriting_From_Existing_Controls/UserControl1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_098_Inheriting_From_Existing_Controls
+{
+    public partial class UserControl1 : Button
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public override string Text {
+            get => base.Text;
+            set
+            {
+                if (value == "Kagaya")
+                {
+                    MessageBox.Show("You are not authorize to use that name");
+                    base.Text = "Usercontrol";
+                }
+                    
+            }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+
+    }
+}
