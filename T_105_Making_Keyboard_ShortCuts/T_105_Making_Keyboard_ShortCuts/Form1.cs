bbff494c867d6ed3404a79ea7463b96f4b16ee39
john <jm_sasuke25@yahoo.com>
2018-02-23 02:08:52 +0900

    Done

---
+++ T_105_Making_Keyboard_ShortCuts/T_105_Making_Keyboard_ShortCuts/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_105_Making_Keyboard_ShortCuts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode.ToString() == "A") ;
+            {
+                MessageBox.Show("User press A");
+            }
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt && e.KeyCode.ToString() == "F") ;
+            {
+                MessageBox.Show("User press F");
+            }
+        }
+    }
+}
