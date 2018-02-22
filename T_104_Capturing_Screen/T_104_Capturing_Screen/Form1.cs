01df589a9ae7334591d536fc15d996ba9d0b1881
john <jm_sasuke25@yahoo.com>
2018-02-22 23:46:31 +0900

    Not Done

--- T_104_Capturing_Screen/T_104_Capturing_Screen/Form1.cs
+++ T_104_Capturing_Screen/T_104_Capturing_Screen/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Threading;
 namespace T_104_Capturing_Screen
 {
     public partial class Form1 : Form
@@ -15,6 +15,14 @@ namespace T_104_Capturing_Screen
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap b = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            Graphics g = Graphics.FromImage(b);
+            g.CopyFromScreen
+
         }
     }
 }
