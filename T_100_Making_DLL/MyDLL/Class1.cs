b796f0dc4b57acb232d85f984a3ca2d406ad9506
john <jm_sasuke25@yahoo.com>
2018-02-22 21:38:42 +0900

    Done

---
+++ T_100_Making_DLL/MyDLL/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDLL
+{   
+    namespace  Client
+    {
+
+        public class Client
+        {
+
+            public string name
+            {
+                get;
+                set;
+            }
+
+            public string Age
+            {
+                get;
+                set;
+            }
+
+        }
+
+
+    }
+    
+}
---
+++ T_100_Making_DLL/T_100_Making_DLL/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Addding Mydll class criented
+using MyDLL.Client;
+namespace T_100_Making_DLL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // this is mydll class
+            
+            Client c = new Client();
+            c.name = "Kagaya";
+            MessageBox.Show(c.name);
+        }
+    }
+}
