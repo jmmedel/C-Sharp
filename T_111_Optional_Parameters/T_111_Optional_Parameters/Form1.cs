b1554f2d21c3cdb9c8854caa07814d9802951c84
john <jm_sasuke25@yahoo.com>
2018-02-24 00:10:33 +0900

    Done
    Done

---
+++ T_111_Optional_Parameters/T_111_Optional_Parameters/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_111_Optional_Parameters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // define it my there name = to the value
+            ShowMessagebox(msg:"Kagaya");
+        }
+        void ShowMessagebox(string msg = "Hello world", string title = "Default value")
+        {
+            MessageBox.Show(msg,title);
+        }
+    }
+}
