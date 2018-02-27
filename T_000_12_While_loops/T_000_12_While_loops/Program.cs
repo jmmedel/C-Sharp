593fbbb46f433f39e76900db6b56886d6f731217
john <jm.kagaya@gmail.com>
2018-02-27 23:36:47 +0900

    Done
    Done

---
+++ T_000_12_While_loops/T_000_12_While_loops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_12_While_loops
+{   /*
+    C# - While Loop
+    A while loop statement in C# repeatedly executes a target statement as long as a given condition is true.
+
+Syntax
+The syntax of a while loop in C# is −
+
+while(condition) {
+   statement(s);
+}
+Here, statement(s) may be a single statement or a block of statements. The condition may be any expression, and true is any non-zero value. The loop iterates while the condition is true.
+
+When the condition becomes false, program control passes to the line immediately following the loop.
+Here, key point of the while loop is that the loop might not ever run. When the condition is tested and the result is false, the loop body is skipped and the first statement after the while loop is executed.
+
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* local variable definition */
+            int a = 10;
+
+            /* while loop execution */
+            while (a < 20)
+            {
+                Console.WriteLine("value of a: {0}", a);
+                a++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
