8a21012eed5b1df3037d63bd721ae57d9b8f75c8
john <jm.kagaya@gmail.com>
2018-02-27 23:55:02 +0900

    Done
    Done

--- T_000_17_Continue/T_000_17_Continue/Program.cs
+++ T_000_17_Continue/T_000_17_Continue/Program.cs
@@ -5,11 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace T_000_17_Continue
-{
+{   /*
+    C# - Continue Statement
+    The continue statement in C# works somewhat like the break statement. Instead of forcing termination, however, continue forces the next iteration of the loop to take place, skipping any code in between.
+
+For the for loop, continue statement causes the conditional test and increment portions of the loop to execute. For the while and do...while loops, continue statement causes the program control passes to the conditional tests.
+
+Syntax
+The syntax for a continue statement in C# is as follows −
+
+continue;
+
+    When the above code is compiled and executed, it produces the following result −
+
+value of a: 10
+value of a: 11
+value of a: 12
+value of a: 13
+value of a: 14
+value of a: 16
+value of a: 17
+value of a: 18
+value of a: 19
+     */
     class Program
     {
         static void Main(string[] args)
         {
+            /* local variable definition */
+            int a = 10;
+
+            /* do loop execution */
+            do
+            {
+                if (a == 15)
+                {
+                    /* skip the iteration */
+                    a = a + 1;
+                    continue;
+                }
+                Console.WriteLine("value of a: {0}", a);
+                a++;
+            }
+            while (a < 20);
+            Console.ReadLine();
         }
     }
 }
---
+++ T_000_18_Encapsulation/T_000_18_Encapsulation/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_18_Encapsulation
+{   /*
+    C# - Encapsulation
+     Encapsulation is defined 'as the process of enclosing one or more items within a physical or logical package'. Encapsulation, in object oriented programming methodology, prevents access to implementation details.
+
+Abstraction and encapsulation are related features in object oriented programming. Abstraction allows making relevant information visible and encapsulation enables a programmer to implement the desired level of abstraction.
+
+Encapsulation is implemented by using access specifiers. An access specifier defines the scope and visibility of a class member. C# supports the following access specifiers −
+
+Public
+Private
+Protected
+Internal
+Protected internal
+Public Access Specifier
+Public access specifier allows a class to expose its member variables and member functions to other functions and objects. Any public member can be accessed from outside the class.
+
+The following example illustrates this −
+When the above code is compiled and executed, it produces the following result −
+
+Length: 4.5
+Width: 3.5
+Area: 15.75
+In the preceding example, the member variables length and width are declared public, so they can be accessed from the function Main() using an instance of the Rectangle class, named r.
+
+The member function Display() and GetArea() can also access these variables directly without using any instance of the class.
+
+The member functions Display() is also declared public, so it can also be accessed from Main() using an instance of the Rectangle class, named r.
+
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Rectangle2 r2 = new Rectangle2();
+            r2.Acceptdetails();
+            r2.Display();
+            Rectangle3 r3 = new Rectangle3();
+            r3.length = 4.5;
+            r3.width = 3.5;
+            r3.Display();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Rectangle
+    {
+        //member variables
+        public double length;
+        public double width;
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle
+
+    /*
+     Private Access Specifier
+Private access specifier allows a class to hide its member variables and member functions from other functions and objects. Only functions of the same class can access its private members. Even an instance of a class cannot access its private members.
+
+The following example illustrates this −
+When the above code is compiled and executed, it produces the following result −
+
+Enter Length:
+4.4
+Enter Width:
+3.3
+Length: 4.4
+Width: 3.3
+Area: 14.52
+In the preceding example, the member variables length and width are declared private, so they cannot be accessed from the function Main(). The member functions AcceptDetails() and Display() can access these variables. Since the member functions AcceptDetails() and Display() are declared public, they can be accessed from Main() using an instance of the Rectangle class, named r.
+     */
+
+    class Rectangle2
+    {
+        //member variables
+        private double length;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle2
+
+    /*
+     Protected Access Specifier
+Protected access specifier allows a child class to access the member variables and member functions of its base class. This way it helps in implementing inheritance. We will discuss this in more details in the inheritance chapter.
+
+Internal Access Specifier
+Internal access specifier allows a class to expose its member variables and member functions to other functions and objects in the current assembly. In other words, any member with internal access specifier can be accessed from any class or method defined within the application in which the member is defined.
+
+The following program illustrates this −
+     */
+    class Rectangle3
+    {
+        //member variables
+        internal double length;
+        internal double width;
+
+        double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle3
+
+    /*
+     When the above code is compiled and executed, it produces the following result −
+
+Length: 4.5
+Width: 3.5
+Area: 15.75
+In the preceding example, notice that the member function GetArea() is not declared with any access specifier. Then what would be the default access specifier of a class member if we don't mention any? It is private.
+
+Protected Internal Access Specifier
+The protected internal access specifier allows a class to hide its member variables and member functions from other class objects and functions, except a child class within the same application. This is also used while implementing inheritance.*/
+
+}
