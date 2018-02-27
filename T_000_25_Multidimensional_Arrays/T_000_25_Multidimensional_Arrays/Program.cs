c0538f9b5536f8ded380f0215794abe1ef00a8b1
john <jm.kagaya@gmail.com>
2018-02-28 02:17:26 +0900

    Done
    Not Done

---
+++ T_000_25_Multidimensional_Arrays/T_000_25_Multidimensional_Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_25_Multidimensional_Arrays
+{   /*
+    C# - Multidimensional Arrays
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
