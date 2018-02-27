ea5b74b94492f67a4c8fbefb8d3c47667d743330
john <jm.kagaya@gmail.com>
2018-02-27 22:27:56 +0900

    Not
    done

---
+++ T_114_Address_Book_Making_UL/T_114_Address_Book_Making_UL/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_114_Address_Book_Making_UL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
