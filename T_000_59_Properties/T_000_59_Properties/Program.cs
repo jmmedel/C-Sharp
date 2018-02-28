86431b40404ff02d27432627f3419fa8f2ec2a1f
john <jm.kagaya@gmail.com>
2018-03-01 06:58:54 +0900

    Done
    Done

---
+++ T_000_59_Properties/T_000_59_Properties/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_59_Properties
+{
+    /*
+     C# - Properties
+         Properties are named members of classes, structures, and interfaces. Member variables or methods in a class or structures are called Fields. Properties are an extension of fields and are accessed using the same syntax. They use accessors through which the values of the private fields can be read, written or manipulated.
+
+Properties do not name the storage locations. Instead, they have accessors that read, write, or compute their values.
+
+For example, let us have a class named Student, with private fields for age, name, and code. We cannot directly access these fields from outside the class scope, but we can have properties for accessing these private fields.
+
+Accessors
+The accessor of a property contains the executable statements that helps in getting (reading or computing) or setting (writing) the property. The accessor declarations can contain a get accessor, a set accessor, or both. For example −
+
+// Declare a Code property of type string:
+public string Code {
+   get {
+      return code;
+   }
+   set {
+      code = value;
+   }
+}
+
+// Declare a Name property of type string:
+public string Name {
+   get {
+      return name;
+   }
+   set {
+      name = value;
+   }
+}
+
+// Declare a Age property of type int:
+public int Age { 
+   get {
+      return age;
+   }
+   set {
+      age = value;
+   }
+}
+    */
+
+    class Student
+    {
+        private string code = "N.A";
+        private string name = "not known";
+        private int age = 0;
+
+        // Declare a Code property of type string:
+        public string Code
+        {
+            get
+            {
+                return code;
+            }
+            set
+            {
+                code = value;
+            }
+        }
+
+        // Declare a Name property of type string:
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        // Declare a Age property of type int:
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public override string ToString()
+        {
+            return "Code = " + Code + ", Name = " + Name + ", Age = " + Age;
+        }
+    }
+
+    class ExampleDemo
+    {
+        public static void Main()
+        {
+
+            // Create a new Student object:
+            Student s = new Student();
+
+            // Setting code, name and the age of the student
+            s.Code = "001";
+            s.Name = "Zara";
+            s.Age = 9;
+            Console.WriteLine("Student Info: {0}", s);
+
+            //let us increase age
+            s.Age += 1;
+            Console.WriteLine("Student Info: {0}", s);
+            Console.ReadKey();
+        }
+    }
+
+    /*
+     When the above code is compiled and executed, it produces the following result −
+
+Student Info: Code = 001, Name = Zara, Age = 9
+Student Info: Code = 001, Name = Zara, Age = 10
+     
+     */
+
+}
