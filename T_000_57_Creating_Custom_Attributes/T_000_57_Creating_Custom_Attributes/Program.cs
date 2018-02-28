24f94e9ecdbb500ec62e6d969c7f39af0f2949fd
john <jm.kagaya@gmail.com>
2018-03-01 06:38:32 +0900

    Done
    Done

--- T_000_57_Creating_Custom_Attributes/T_000_57_Creating_Custom_Attributes/Program.cs
+++ T_000_57_Creating_Custom_Attributes/T_000_57_Creating_Custom_Attributes/Program.cs
@@ -3,16 +3,174 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Reflection;
 namespace T_000_57_Creating_Custom_Attributes
 {   /*
-    
+    Creating Custom Attributes
+The .Net Framework allows creation of custom attributes that can be used to store declarative information and can be retrieved at run-time. This information can be related to any target element depending upon the design criteria and application need.
+
+Creating and using custom attributes involve four steps −
+
+Declaring a custom attribute
+Constructing the custom attribute
+Apply the custom attribute on a target program element
+Accessing Attributes Through Reflection
+The Last step involves writing a simple program to read through the metadata to find various notations. Metadata is data about data or information used for describing other data. This program should use reflections for accessing attributes at runtime. This we will discuss in the next chapter.
+
+Declaring a Custom Attribute
+A new custom attribute should is derived from the System.Attribute class. For example,
+
+//a custom attribute BugFix to be assigned to a class and its members
+[AttributeUsage(
+   AttributeTargets.Class |
+   AttributeTargets.Constructor |
+   AttributeTargets.Field |
+   AttributeTargets.Method |
+   AttributeTargets.Property,
+   AllowMultiple = true)]
+
+public class DeBugInfo : System.Attribute
+In the preceding code, we have declared a custom attribute named DeBugInfo.
+
+Constructing the Custom Attribute
+Let us construct a custom attribute named DeBugInfo, which stores the information obtained by debugging any program. Let it store the following information −
+
+The code number for the bug
+Name of the developer who identified the bug
+Date of last review of the code
+A string message for storing the developer's remarks
+The DeBugInfo class has three private properties for storing the first three information and a public property for storing the message. Hence the bug number, developer's name, and date of review are the positional parameters of the DeBugInfo class and the message is an optional or named parameter.
+
+Each attribute must have at least one constructor. The positional parameters should be passed through the constructor. The following code shows the DeBugInfo class −
+
+
      
      */
-    class Program
+
+    //a custom attribute BugFix to be assigned to a class and its members
+    [AttributeUsage(
+       AttributeTargets.Class |
+       AttributeTargets.Constructor |
+       AttributeTargets.Field |
+       AttributeTargets.Method |
+       AttributeTargets.Property,
+       AllowMultiple = true)]
+
+    public class DeBugInfo : System.Attribute
+    {
+        private int bugNo;
+        private string developer;
+        private string lastReview;
+        public string message;
+
+        public DeBugInfo(int bg, string dev, string d)
+        {
+            this.bugNo = bg;
+            this.developer = dev;
+            this.lastReview = d;
+        }
+        public int BugNo
+        {
+            get
+            {
+                return bugNo;
+            }
+        }
+        public string Developer
+        {
+            get
+            {
+                return developer;
+            }
+        }
+        public string LastReview
+        {
+            get
+            {
+                return lastReview;
+            }
+        }
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+            }
+        }
+    }
+
+    [DeBugInfo(45, "Zara Ali", "12/8/2012", Message = "Return type mismatch")]
+    [DeBugInfo(49, "Nuha Ali", "10/10/2012", Message = "Unused variable")]
+    class Rectangle
+    {
+        //member variables
+        protected double length;
+        protected double width;
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        [DeBugInfo(55, "Zara Ali", "19/10/2012", Message = "Return type mismatch")]
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+        [DeBugInfo(56, "Zara Ali", "19/10/2012")]
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+
+    class ExecuteRectangle
     {
         static void Main(string[] args)
         {
+            Rectangle r = new Rectangle(4.5, 7.5);
+            r.Display();
+            Type type = typeof(Rectangle);
+
+            //iterating through the attribtues of the Rectangle class
+            foreach (Object attributes in type.GetCustomAttributes(false))
+            {
+                DeBugInfo dbi = (DeBugInfo)attributes;
+
+                if (null != dbi)
+                {
+                    Console.WriteLine("Bug no: {0}", dbi.BugNo);
+                    Console.WriteLine("Developer: {0}", dbi.Developer);
+                    Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
+                    Console.WriteLine("Remarks: {0}", dbi.Message);
+                }
+            }
+
+            //iterating through the method attribtues
+            foreach (MethodInfo m in type.GetMethods())
+            {
+
+                foreach (Attribute a in m.GetCustomAttributes(true))
+                {
+                    DeBugInfo dbi = (DeBugInfo)a;
+
+                    if (null != dbi)
+                    {
+                        Console.WriteLine("Bug no: {0}, for Method: {1}", dbi.BugNo, m.Name);
+                        Console.WriteLine("Developer: {0}", dbi.Developer);
+                        Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
+                        Console.WriteLine("Remarks: {0}", dbi.Message);
+                    }
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
