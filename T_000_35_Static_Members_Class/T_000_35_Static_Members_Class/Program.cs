53ef3653112fb1a866e40fa99d2de81ba30198f9
john <jm.kagaya@gmail.com>
2018-02-28 22:02:33 +0900

    Done
    Done

--- T_000_35_Static_Members_Class/T_000_35_Static_Members_Class/Program.cs
+++ T_000_35_Static_Members_Class/T_000_35_Static_Members_Class/Program.cs
@@ -5,11 +5,85 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace T_000_35_Static_Members_Class
-{
-    class Program
+{   /*
+    Static Members of a C# Class
+We can define class members as static using the static keyword. When we declare a member of a class as static, it means no matter how many objects of the class are created, there is only one copy of the static member.
+
+The keyword static implies that only one instance of the member exists for a class. Static variables are used for defining constants because their values can be retrieved by invoking the class without creating an instance of it. Static variables can be initialized outside the member function or class definition. You can also initialize static variables inside the class definition.
+
+The following example demonstrates the use of static variables −
+     */
+
+    class StaticVar
+    {
+        public static int num;
+
+        public void count()
+        {
+            num++;
+        }
+        public int getNum()
+        {
+            return num;
+        }
+    }
+    class StaticTester
     {
         static void Main(string[] args)
         {
+            StaticVar s1 = new StaticVar();
+            StaticVar s2 = new StaticVar();
+
+            s1.count();
+            s1.count();
+            s1.count();
+
+            s2.count();
+            s2.count();
+            s2.count();
+
+            Console.WriteLine("Variable num for s1: {0}", s1.getNum());
+            Console.WriteLine("Variable num for s2: {0}", s2.getNum());
+            Console.ReadKey();
         }
     }
+
+    /*
+     When the above code is compiled and executed, it produces the following result −
+
+Variable num for s1: 6
+Variable num for s2: 6
+You can also declare a member function as static. Such functions can access only static variables. The static functions exist even before the object is created. The following example demonstrates the use of static functions −
+using System;
+
+namespace StaticVarApplication {
+   class StaticVar {
+      public static int num;
+      
+      public void count() {
+         num++;
+      }
+      public static int getNum() {
+         return num;
+      }
+   }
+   class StaticTester {
+      static void Main(string[] args) {
+         StaticVar s = new StaticVar();
+         
+         s.count();
+         s.count();
+         s.count();
+         
+         Console.WriteLine("Variable num: {0}", StaticVar.getNum());
+         Console.ReadKey();
+      }
+   }
+}
+When the above code is compiled and executed, it produces the following result −
+
+Variable num: 3
+     
+     */
+
 }
