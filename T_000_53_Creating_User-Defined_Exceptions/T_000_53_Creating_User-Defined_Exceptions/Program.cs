e902afcbe0b51f3d85ef4e78d87e053930edf5f9
john <jm.kagaya@gmail.com>
2018-03-01 03:26:21 +0900

    Done
    Done

---
+++ T_000_53_Creating_User-Defined_Exceptions/T_000_53_Creating_User-Defined_Exceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_53_Creating_User_Defined_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
