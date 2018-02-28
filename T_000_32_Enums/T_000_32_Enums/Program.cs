a4d9f2b228509a12f42459fcb4954563becf3c62
john <jm.kagaya@gmail.com>
2018-02-28 21:48:37 +0900

    Done
    Done

--- T_000_32_Enums/T_000_32_Enums/Program.cs
+++ T_000_32_Enums/T_000_32_Enums/Program.cs
@@ -5,11 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace T_000_32_Enums
-{
+{ /* C# - Enums
+    An enumeration is a set of named integer constants. An enumerated type is declared using the enum keyword.
+
+C# enumerations are value data type. In other words, enumeration contains its own values and cannot inherit or cannot pass inheritance.
+
+Declaring enum Variable
+The general syntax for declaring an enumeration is −
+
+enum <enum_name> {
+   enumeration list 
+};
+Where,
+
+The enum_name specifies the enumeration type name.
+
+The enumeration list is a comma-separated list of identifiers.
+
+Each of the symbols in the enumeration list stands for an integer value, one greater than the symbol that precedes it. By default, the value of the first enumeration symbol is 0. For example −
+
+enum Days { Sun, Mon, tue, Wed, thu, Fri, Sat };
+  */
     class Program
     {
+        enum Days { Sun, Mon, tue, Wed, thu, Fri, Sat };
         static void Main(string[] args)
         {
+            int WeekdayStart = (int)Days.Mon;
+            int WeekdayEnd = (int)Days.Fri;
+
+            Console.WriteLine("Monday: {0}", WeekdayStart);
+            Console.WriteLine("Friday: {0}", WeekdayEnd);
+            Console.ReadKey();
         }
     }
 }
