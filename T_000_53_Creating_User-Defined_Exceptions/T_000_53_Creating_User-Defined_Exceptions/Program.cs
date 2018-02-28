4edda36807de21012e48ac88daf7b061001dc69b
john <jm.kagaya@gmail.com>
2018-03-01 03:27:44 +0900

    Done
    Done

--- T_000_53_Creating_User-Defined_Exceptions/T_000_53_Creating_User-Defined_Exceptions/Program.cs
+++ T_000_53_Creating_User-Defined_Exceptions/T_000_53_Creating_User-Defined_Exceptions/Program.cs
@@ -6,10 +6,61 @@ using System.Threading.Tasks;
 
 namespace T_000_53_Creating_User_Defined_Exceptions
 {
-    class Program
+    /*
+     Creating User-Defined Exceptions
+You can also define your own exception. User-defined exception classes are derived from the Exception class. The following example demonstrates this −
+     */
+
+    class TestTemperature
     {
         static void Main(string[] args)
         {
+            Temperature temp = new Temperature();
+            try
+            {
+                temp.showTemp();
+            }
+            catch (TempIsZeroException e)
+            {
+                Console.WriteLine("TempIsZeroException: {0}", e.Message);
+            }
+            Console.ReadKey();
         }
     }
+}
+public class TempIsZeroException : Exception
+{
+    public TempIsZeroException(string message) : base(message)
+    {
+    }
+}
+public class Temperature
+{
+    int temperature = 0;
+
+    public void showTemp()
+    {
+
+        if (temperature == 0)
+        {
+            throw (new TempIsZeroException("Zero Temperature found"));
+        }
+        else
+        {
+            Console.WriteLine("Temperature: {0}", temperature);
+        }
+    }
+
+    /*
+     When the above code is compiled and executed, it produces the following result −
+
+TempIsZeroException: Zero Temperature found
+Throwing Objects
+You can throw an object if it is either directly or indirectly derived from the System.Exception class. You can use a throw statement in the catch block to throw the present object as −
+
+Catch(Exception e) {
+   ...
+   Throw e
+}
+     */
 }
