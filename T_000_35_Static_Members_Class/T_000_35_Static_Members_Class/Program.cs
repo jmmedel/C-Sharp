69b20d303dab7a27ee1adb3b7674e47c278e6f0a
john <jm.kagaya@gmail.com>
2018-02-28 22:00:00 +0900

    Done
    Done

---
+++ T_000_35_Static_Members_Class/T_000_35_Static_Members_Class/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_35_Static_Members_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
