64c22e9312fb30adad47f1835da34ea66dab7045
john <jm.kagaya@gmail.com>
2018-03-01 15:50:23 +0900

    Done
    Done

--- T_000_65_Events/T_000_65_Events/Program.cs
+++ T_000_65_Events/T_000_65_Events/Program.cs
@@ -6,12 +6,51 @@ using System.Threading.Tasks;
 
 namespace T_000_65_Events
 {   /*
-    
+    C# - Events
+    Events are user actions such as key press, clicks, mouse movements, etc., or some occurrence such as system generated notifications. Applications need to respond to events when they occur. For example, interrupts. Events are used for inter-process communication.
+
+Using Delegates with Events
+The events are declared and raised in a class and associated with the event handlers using delegates within the same class or some other class. The class containing the event is used to publish the event. This is called the publisher class. Some other class that accepts this event is called the subscriber class. Events use the publisher-subscriber model.
+
+A publisher is an object that contains the definition of the event and the delegate. The event-delegate association is also defined in this object. A publisher class object invokes the event and it is notified to other objects.
+
+A subscriber is an object that accepts the event and provides an event handler. The delegate in the publisher class invokes the method (event handler) of the subscriber class.
+
+Declaring Events
+To declare an event inside a class, first a delegate type for the event must be declared. For example,
+
+public delegate string MyDel(string str);
+Next, the event itself is declared, using the event keyword −
+
+event MyDel MyEvent;
+The preceding code defines a delegate named BoilerLogHandler and an event named BoilerEventLog, which invokes the delegate when it is raised.
      */
-    class Program
+
+    namespace SampleApp
     {
-        static void Main(string[] args)
+        public delegate string MyDel(string str);
+
+        class EventProgram
         {
+            event MyDel MyEvent;
+
+            public EventProgram()
+            {
+                this.MyEvent += new MyDel(this.WelcomeUser);
+            }
+            public string WelcomeUser(string username)
+            {
+                return "Welcome " + username;
+            }
+            static void Main(string[] args)
+            {
+                EventProgram obj1 = new EventProgram();
+                string result = obj1.MyEvent("Hello World");
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
         }
     }
+
+
 }
---
+++ T_000_66_Collections/T_000_66_Collections/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_66_Collections
+{   /*
+    C# - Collections
+    Collection classes are specialized classes for data storage and retrieval. These classes provide support for stacks, queues, lists, and hash tables. Most collection classes implement the same interfaces.
+
+Collection classes serve various purposes, such as allocating memory dynamically to elements and accessing a list of items on the basis of an index etc. These classes create collections of objects of the Object class, which is the base class for all data types in C#.
+
+Various Collection Classes and Their Usage
+The following are the various commonly used classes of the System.Collection namespace. Click the following links to check their detail.
+
+Sr.No.	Class & Description and Useage
+1	ArrayList
+It represents ordered collection of an object that can be indexed individually.
+
+It is basically an alternative to an array. However, unlike array you can add and remove items from a list at a specified position using an index and the array resizes itself automatically. It also allows dynamic memory allocation, adding, searching and sorting items in the list.
+
+2	Hashtable
+It uses a key to access the elements in the collection.
+
+A hash table is used when you need to access elements by using key, and you can identify a useful key value. Each item in the hash table has a key/value pair. The key is used to access the items in the collection.
+
+3	SortedList
+It uses a key as well as an index to access the items in a list.
+
+A sorted list is a combination of an array and a hash table. It contains a list of items that can be accessed using a key or an index. If you access items using an index, it is an ArrayList, and if you access items using a key , it is a Hashtable. The collection of items is always sorted by the key value.
+
+4	Stack
+It represents a last-in, first out collection of object.
+
+It is used when you need a last-in, first-out access of items. When you add an item in the list, it is called pushing the item and when you remove it, it is called popping the item.
+
+5	Queue
+It represents a first-in, first out collection of object.
+
+It is used when you need a first-in, first-out access of items. When you add an item in the list, it is called enqueue and when you remove an item, it is called deque.
+
+6	BitArray
+It represents an array of the binary representation using the values 1 and 0.
+
+It is used when you need to store the bits but do not know the number of bits in advance. You can access items from the BitArray collection by using an integer index, which starts from zero.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
