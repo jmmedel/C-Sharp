7314b0993eacb38d4e2bc6abd6ffdef0b5da81d2
john <jm.kagaya@gmail.com>
2018-03-01 17:06:58 +0900

    Done
    Done

--- T_000_81_Managing_Threads/T_000_81_Managing_Threads/Program.cs
+++ T_000_81_Managing_Threads/T_000_81_Managing_Threads/Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 namespace T_000_81_Managing_Threads
-{
+{   /*
+    Managing Threads
+The Thread class provides various methods for managing threads.
+
+The following example demonstrates the use of the sleep() method for making a thread pause for a specific period of time.
+
+     */
     class Program
-    {
+    {   
+        public static void CalltoChildThread()
+        {
+            Console.WriteLine("Child threads start");
+            // the read is paused for 5000miliseconds
+            int sleepfor = 5000;
+            Console.WriteLine("The child thread paused for {0} seconds",sleepfor/1000);
+            Thread.Sleep(sleepfor);
+            Console.WriteLine("The child thread resumes");
+        }
+
         static void Main(string[] args)
         {
+            ThreadStart childeref = new ThreadStart(CalltoChildThread);
+            Console.WriteLine("In Main: Creating the Child Thread");
+            Thread childthread = new Thread(childeref);
+            childthread.Start();
+            Console.ReadLine();
+
         }
+        /*
+         When the above code is compiled and executed, it produces the following result −
+
+In Main: Creating the Child thread
+Child thread starts
+Child Thread Paused for 5 seconds
+Child thread resumes
+         */
     }
 }
---
+++ T_000_82_Destroying_Threads/T_000_82_Destroying_Threads/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace T_000_82_Destroying_Threads
+{   /*
+    Destroying Threads
+The Abort() method is used for destroying threads.
+
+The runtime aborts the thread by throwing a ThreadAbortException. This exception cannot be caught, the control is sent to the finally block, if any.
+
+The following program illustrates this −
+     */
+    namespace MultithreadingApplication
+    {
+        class ThreadCreationProgram
+        {
+            public static void CallToChildThread()
+            {
+                try
+                {
+                    Console.WriteLine("Child thread starts");
+
+                    // do some work, like counting to 10
+                    for (int counter = 0; counter <= 10; counter++)
+                    {
+                        Thread.Sleep(500);
+                        Console.WriteLine(counter);
+                    }
+
+                    Console.WriteLine("Child Thread Completed");
+                }
+                catch (ThreadAbortException e)
+                {
+                    Console.WriteLine("Thread Abort Exception");
+                }
+                finally
+                {
+                    Console.WriteLine("Couldn't catch the Thread Exception");
+                }
+            }
+            static void Main(string[] args)
+            {
+                ThreadStart childref = new ThreadStart(CallToChildThread);
+                Console.WriteLine("In Main: Creating the Child thread");
+
+                Thread childThread = new Thread(childref);
+                childThread.Start();
+
+                //stop the main thread for some time
+                Thread.Sleep(2000);
+
+                //now abort the child
+                Console.WriteLine("In Main: Aborting the Child thread");
+
+                childThread.Abort();
+                Console.ReadKey();
+            }
+        }
+    }
+    /*
+     When the above code is compiled and executed, it produces the following result −
+
+In Main: Creating the Child thread
+Child thread starts
+0
+1
+2
+In Main: Aborting the Child thread
+Thread Abort Exception
+Couldn't catch the Thread Exception 
+
+     */
+}
