015931813e689805fcbe8be67052d99e6b661e28
john <jm.kagaya@gmail.com>
2018-03-01 16:22:06 +0900

    Done
    Done

---
+++ T_000_72_BitArray_Class/T_000_72_BitArray_Class/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_72_BitArray_Class
+{ /*
+    C# - BitArray Class
+    The BitArray class manages a compact array of bit values, which are represented as Booleans, where true indicates that the bit is on (1) and false indicates the bit is off (0).
+
+It is used when you need to store the bits but do not know the number of bits in advance. You can access items from the BitArray collection by using an integer index, which starts from zero.
+
+Methods and Properties of the BitArray Class
+The following table lists some of the commonly used properties of the BitArray class −
+
+Sr.No.	Property & Description
+1	
+Count
+
+Gets the number of elements contained in the BitArray.
+
+2	
+IsReadOnly
+
+Gets a value indicating whether the BitArray is read-only.
+
+3	
+Item
+
+Gets or sets the value of the bit at a specific position in the BitArray.
+
+4	
+Length
+
+Gets or sets the number of elements in the BitArray.
+
+The following table lists some of the commonly used methods of the BitArray class −
+
+Sr.No.	Method & Description
+1	
+public BitArray And(BitArray value);
+
+Performs the bitwise AND operation on the elements in the current BitArray against the corresponding elements in the specified BitArray.
+
+2	
+public bool Get(int index);
+
+Gets the value of the bit at a specific position in the BitArray.
+
+3	
+public BitArray Not();
+
+Inverts all the bit values in the current BitArray, so that elements set to true are changed to false, and elements set to false are changed to true.
+
+4	
+public BitArray Or(BitArray value);
+
+Performs the bitwise OR operation on the elements in the current BitArray against the corresponding elements in the specified BitArray.
+
+5	
+public void Set(int index, bool value);
+
+Sets the bit at a specific position in the BitArray to the specified value.
+
+6	
+public void SetAll(bool value);
+
+Sets all bits in the BitArray to the specified value.
+
+7	
+public BitArray Xor(BitArray value);
+
+Performs the bitwise eXclusive OR operation on the elements in the current BitArray against the corresponding elements in the specified BitArray.
+     */
+    using System.Collections;
+    namespace CollectionsApplication
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //creating two  bit arrays of size 8
+                BitArray ba1 = new BitArray(8);
+                BitArray ba2 = new BitArray(8);
+
+                byte[] a = { 1 };
+                byte[] b = { 13 };
+
+                //storing the values 60, and 13 into the bit arrays
+                ba1 = new BitArray(a);
+                ba2 = new BitArray(b);
+
+                //content of ba1
+                Console.WriteLine("Bit array ba1: 60");
+
+                for (int i = 0; i < ba1.Count; i++)
+                {
+                    Console.Write("{0, -6} ", ba1[i]);
+                }
+                Console.WriteLine();
+
+                //content of ba2
+                Console.WriteLine("Bit array ba2: 13");
+
+                for (int i = 0; i < ba2.Count; i++)
+                {
+                    Console.Write("{0, -6} ", ba2[i]);
+                }
+                Console.WriteLine();
+                BitArray ba3 = new BitArray(8);
+                ba3 = ba1.And(ba2);
+
+                //content of ba3
+                Console.WriteLine("Bit array ba3 after AND operation: 12");
+
+                for (int i = 0; i < ba3.Count; i++)
+                {
+                    Console.Write("{0, -6} ", ba3[i]);
+                }
+                Console.WriteLine();
+                ba3 = ba1.Or(ba2);
+
+                //content of ba3
+                Console.WriteLine("Bit array ba3 after OR operation: 61");
+
+                for (int i = 0; i < ba3.Count; i++)
+                {
+                    Console.Write("{0, -6} ", ba3[i]);
+                }
+                Console.WriteLine();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ T_000_73_Generics/T_000_73_Generics/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_000_73_Generics
+{   /*
+    C# - Generics
+    Generics allow you to delay the specification of the 
+    data type of programming elements in a class or a method,
+    until it is actually used in the program. In other words, 
+    generics allow you to write a class or method that can work 
+    with any data type.
+
+You write the specifications for the class
+or the method, with substitute parameters for 
+data types. When the compiler encounters a constructor 
+for the class or a function call for the method, it generates 
+code to handle the specific data type. A simple example would 
+help understanding the concept −
+     */
+    using System.Collections.Generic;
+
+    namespace GenericApplication
+    {
+        public class MyGenericArray<T>
+        {
+            private T[] array;
+
+            public MyGenericArray(int size)
+            {
+                array = new T[size + 1];
+            }
+            public T getItem(int index)
+            {
+                return array[index];
+            }
+            public void setItem(int index, T value)
+            {
+                array[index] = value;
+            }
+        }
+        class Tester
+        {
+            static void Main(string[] args)
+            {
+
+                //declaring an int array
+                MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+
+                //setting values
+                for (int c = 0; c < 5; c++)
+                {
+                    intArray.setItem(c, c * 5);
+                }
+
+                //retrieving the values
+                for (int c = 0; c < 5; c++)
+                {
+                    Console.Write(intArray.getItem(c) + " ");
+                }
+
+                Console.WriteLine();
+
+                //declaring a character array
+                MyGenericArray<char> charArray = new MyGenericArray<char>(5);
+
+                //setting values
+                for (int c = 0; c < 5; c++)
+                {
+                    charArray.setItem(c, (char)(c + 97));
+                }
+
+                //retrieving the values
+                for (int c = 0; c < 5; c++)
+                {
+                    Console.Write(charArray.getItem(c) + " ");
+                }
+                Console.WriteLine();
+
+                Console.ReadKey();
+            }
+        }
+    }
+    /*
+     When the above code is compiled and executed, it produces the following result −
+
+0 5 10 15 20
+a b c d e
+Features of Generics
+Generics is a technique that enriches your programs in the following ways −
+
+It helps you to maximize code reuse, type safety, and performance.
+
+You can create generic collection classes. The .NET Framework class library contains several new generic collection classes in the System.Collections.Generic namespace. You may use these generic collection classes instead of the collection classes in the System.Collections namespace.
+
+You can create your own generic interfaces, classes, methods, events, and delegates.
+
+You may create generic classes constrained to enable access to methods on particular data types.
+
+You may get information on the types used in a generic data type at run-time by means of reflection.
+     */
+
+}
