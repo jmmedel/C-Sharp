3597eb8882a062269e93407ce1ed837c2510a1c3
john <jm.kagaya@gmail.com>
2018-03-02 00:16:39 +0900

    Done
    Done

--- T_014_WPF_Datagrid/T_014_WPF_Datagrid/MainWindow.xaml.cs
+++ T_014_WPF_Datagrid/T_014_WPF_Datagrid/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,13 +20,141 @@ using System.Windows.Shapes;
 namespace T_014_WPF_Datagrid
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml 
     /// </summary>
+
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            dataGrid.ItemsSource = Employee.GetEmployees();
         }
+    }
+
+    public enum Party
+    {
+        Indepentent,
+        Federalist,
+        DemocratRepublican,
+    }
+
+    public class Employee : INotifyPropertyChanged
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                RaiseProperChanged();
+            }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                RaiseProperChanged();
+            }
+        }
+
+        private bool wasReElected;
+
+        public bool WasReElected
+        {
+            get { return wasReElected; }
+            set
+            {
+                wasReElected = value;
+                RaiseProperChanged();
+            }
+        }
+
+        private Party affiliation;
+
+        public Party Affiliation
+        {
+            get { return affiliation; }
+            set
+            {
+                affiliation = value;
+                RaiseProperChanged();
+            }
+        }
+
+        public static ObservableCollection<Employee> GetEmployees()
+        {
+            var employees = new ObservableCollection<Employee>();
+
+            employees.Add(new Employee()
+            {
+                Name = "Ali",
+                Title = "Minister",
+                WasReElected = true,
+                Affiliation = Party.Indepentent
+            });
+
+            employees.Add(new Employee()
+            {
+                Name = "Ahmed",
+                Title = "CM",
+                WasReElected = false,
+                Affiliation = Party.Federalist
+            });
+
+            employees.Add(new Employee()
+            {
+                Name = "Amjad",
+                Title = "PM",
+                WasReElected = true,
+                Affiliation = Party.DemocratRepublican
+            });
+
+            employees.Add(new Employee()
+            {
+                Name = "Waqas",
+                Title = "Minister",
+                WasReElected = false,
+                Affiliation = Party.Indepentent
+            });
+
+            employees.Add(new Employee()
+            {
+                Name = "Bilal",
+                Title = "Minister",
+                WasReElected = true,
+                Affiliation = Party.Federalist
+            });
+
+            employees.Add(new Employee()
+            {
+                Name = "Waqar",
+                Title = "Minister",
+                WasReElected = false,
+                Affiliation = Party.DemocratRepublican
+            });
+
+            return employees;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaiseProperChanged([CallerMemberName] string caller = "")
+        {
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+
     }
 }
