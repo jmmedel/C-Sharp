2892f134cf829ec80e22a29eb69bf8a59670c046
john <jm.kagaya@gmail.com>
2018-03-01 16:27:25 +0900

    Done
    Done

---
+++ T_000_74_Generic_Methods/T_000_Generic_Methods/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace T_000_Generic_Methods
+{   /*
+    Generic Methods
+In the previous example, we have used a generic 
+class; we can declare a generic method with a type 
+parameter. The following program illustrates the concept −
+
+     */
+
+    class Program
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b;
+            char c, d;
+            a = 10;
+            b = 20;
+            c = 'I';
+            d = 'V';
+            //display values before swap:
+            Console.WriteLine("Int values before calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Char values before calling swap:");
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+
+            //call swap
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c, ref d);
+
+            //display values after swap:
+            Console.WriteLine("Int values after calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Char values after calling swap:");
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+
+            Console.ReadKey();
+        }
+
+    }
+    /*
+     When the above code is compiled and executed, it produces the following result −
+
+Int values before calling swap:
+a = 10, b = 20
+Char values before calling swap:
+c = I, d = V
+Int values after calling swap:
+a = 20, b = 10
+Char values after calling swap:
+c = V, d = I
+     */
+}
