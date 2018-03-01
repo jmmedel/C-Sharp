1825e46b667cae99160b90c42f9c8a5b3b7b2a5d
john <jm.kagaya@gmail.com>
2018-03-01 14:49:47 +0900

    Done
    Done

--- T_000_62_Delegates/T_000_62_Delegates/Program.cs
+++ T_000_62_Delegates/T_000_62_Delegates/Program.cs
@@ -7,13 +7,62 @@ using System.Threading.Tasks;
 namespace T_000_62_Delegates
 {
     /*
-     
-         
+     C# - Delegates
+         C# delegates are similar to pointers to functions, in C or C++. A delegate is a reference type variable that holds the reference to a method. The reference can be changed at runtime.
+
+Delegates are especially used for implementing events and the call-back methods. All delegates are implicitly derived from the System.Delegate class.
+
+Declaring Delegates
+Delegate declaration determines the methods that can be referenced by the delegate. A delegate can refer to a method, which has the same signature as that of the delegate.
+
+For example, consider a delegate −
+
+public delegate int MyDelegate (string s);
+The preceding delegate can be used to reference any method that has a single string parameter and returns an int type variable.
+
+Syntax for delegate declaration is −
+
+delegate <return type> <delegate-name> <parameter list>
+Instantiating Delegates
+Once a delegate type is declared, a delegate object must be created with the new keyword and be associated with a particular method. When creating a delegate, the argument passed to the new expression is written similar to a method call, but without the arguments to the method. For example −
+
+public delegate void printString(string s);
+...
+printString ps1 = new printString(WriteToScreen);
+printString ps2 = new printString(WriteToFile);
+Following example demonstrates declaration, instantiation, and use of a delegate that can be used to reference methods that take an integer parameter and returns an integer value.
     */
+    delegate int NumberChanger(int n);
     class Program
     {
+        static int num = 20;
+        public static int Addnum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+
         static void Main(string[] args)
         {
+            //create delegate instances
+            NumberChanger nc1 = new NumberChanger(Addnum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            //calling the methods using the delegate objects
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
         }
     }
 }
