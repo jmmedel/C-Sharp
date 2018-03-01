4eb9c5225236ba34e46672fe369eee0383f2d54f
john <jm.kagaya@gmail.com>
2018-03-01 23:33:07 +0900

    Done
    Done

--- T_011_WPF_Controls_CheckBox/T_011_WPF_Controls_CheckBox/MainWindow.xaml.cs
+++ T_011_WPF_Controls_CheckBox/T_011_WPF_Controls_CheckBox/MainWindow.xaml.cs
@@ -24,5 +24,27 @@ namespace T_011_WPF_Controls_CheckBox
         {
             InitializeComponent();
         }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            if (cb.Name == "checkBox1") textBox1.Text = "2 state CheckBox is checked.";
+            else textBox2.Text = "3 state CheckBox is checked.";
+        }
+
+        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            if (cb.Name == "checkBox1") textBox1.Text = "2 state CheckBox is unchecked.";
+            else textBox2.Text = "3 state CheckBox is unchecked.";
+        }
+
+        private void HandleThirdState(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            textBox2.Text = "3 state CheckBox is in indeterminate state.";
+        }
     }
 }
