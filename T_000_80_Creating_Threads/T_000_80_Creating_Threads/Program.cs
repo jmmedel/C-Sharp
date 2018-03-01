ff2f583e9a8f35a5e8bdf17fec83c2aa6f2843c4
john <jm.kagaya@gmail.com>
2018-03-01 16:54:46 +0900

    Done
    Done

--- T_000_80_Creating_Threads/T_000_80_Creating_Threads/Program.cs
+++ T_000_80_Creating_Threads/T_000_80_Creating_Threads/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 namespace T_000_80_Creating_Threads
-{
-    class Program
+{   /*
+    Creating Threads
+Threads are created by extending the Thread class. The extended Thread class then calls the Start() method to begin the child thread execution.
+
+The following program demonstrates the concept −
+
+     */
+    namespace MultithreadingApplication
     {
-        static void Main(string[] args)
+        class ThreadCreationProgram
         {
+            public static void CallToChildThread()
+            {
+                Console.WriteLine("Child thread starts");
+            }
+            static void Main(string[] args)
+            {
+                ThreadStart childref = new ThreadStart(CallToChildThread);
+                Console.WriteLine("In Main: Creating the Child thread");
+                Thread childThread = new Thread(childref);
+                childThread.Start();
+                Console.ReadKey();
+            }
         }
     }
 }
