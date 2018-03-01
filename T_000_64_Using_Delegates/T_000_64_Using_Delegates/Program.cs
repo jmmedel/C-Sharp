cde75c5d5766b5356d47fa64a316c870e69b8a20
john <jm.kagaya@gmail.com>
2018-03-01 14:55:02 +0900

    Done
    Done

--- T_000_64_Using_Delegates/T_000_64_Using_Delegates/Program.cs
+++ T_000_64_Using_Delegates/T_000_64_Using_Delegates/Program.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace T_000_64_Using_Delegates
-{
-    class Program
+{   /*
+    Using Delegates
+The following example demonstrates the use of delegate. The delegate printString can be used to reference method that takes a string as input and returns nothing.
+
+We use this delegate to call two methods, the first prints the string to the console, and the second one prints it to a file −
+     */
+    namespace DelegateAppl
     {
-        static void Main(string[] args)
+
+        class PrintString
         {
+            static FileStream fs;
+            static StreamWriter sw;
+
+            // delegate declaration
+            public delegate void printString(string s);
+
+            // this method prints to the console
+            public static void WriteToScreen(string str)
+            {
+                Console.WriteLine("The String is: {0}", str);
+            }
+
+            //this method prints to a file
+            public static void WriteToFile(string s)
+            {
+                fs = new FileStream("c:\\message.txt",
+                FileMode.Append, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                sw.WriteLine(s);
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+
+            // this method takes the delegate as parameter and uses it to
+            // call the methods as required
+            public static void sendString(printString ps)
+            {
+                ps("Hello World");
+            }
+
+            static void Main(string[] args)
+            {
+                printString ps1 = new printString(WriteToScreen);
+                printString ps2 = new printString(WriteToFile);
+                sendString(ps1);
+                sendString(ps2);
+                Console.ReadKey();
+            }
         }
     }
 }
