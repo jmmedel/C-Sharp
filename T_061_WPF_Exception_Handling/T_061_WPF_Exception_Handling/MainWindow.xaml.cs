ad7687b9640e4b312325ded0f05a2ad093bf4b93
john <jm.kagaya@gmail.com>
2018-03-02 22:27:45 +0900

    Done
    Done

--- T_061_WPF_Exception_Handling/T_061_WPF_Exception_Handling/MainWindow.xaml.cs
+++ T_061_WPF_Exception_Handling/T_061_WPF_Exception_Handling/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
 namespace T_061_WPF_Exception_Handling
 {
     /// <summary>
@@ -23,6 +23,33 @@ namespace T_061_WPF_Exception_Handling
         public MainWindow()
         {
             InitializeComponent();
+            ReadFile(0);
         }
+        void ReadFile(int index)
+        {
+            string path = ""; // type the path here
+            StreamReader file = new StreamReader(path);
+            char[] buffer = new char[80];
+
+            try
+            {
+                file.ReadBlock(buffer, index, buffer.Length);
+                string str = new string(buffer);
+                str.Trim();
+                textBox.Text = str;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error reading from " + path + "\nMessage = " + e.Message);
+            }
+            finally
+            {
+                if (file != null)
+                {
+                    file.Close();
+                }
+            }
+        }
+
     }
 }
