264eb0f114f5077aae863609ed99daaef327acd3
john <jm.kagaya@gmail.com>
2018-03-02 19:09:52 +0900

    Done
    Done

---
+++ T_034_WPF_Dock_Panel/T_034_WPF_Dock_Panel/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T_034_WPF_Dock_Panel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Click_Me(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string str = btn.Content.ToString() + " button clicked";
+            MessageBox.Show(str);
+        }
+
+    }
+}
