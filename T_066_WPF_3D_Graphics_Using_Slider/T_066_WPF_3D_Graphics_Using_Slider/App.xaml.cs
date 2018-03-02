4f71e09e2cd0cbd2a12a5308511fc26b2131e447
john <jm.kagaya@gmail.com>
2018-03-02 22:41:07 +0900

    Done
    Done

---
+++ T_066_WPF_3D_Graphics_Using_Slider/T_066_WPF_3D_Graphics_Using_Slider/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace T_066_WPF_3D_Graphics_Using_Slider
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
