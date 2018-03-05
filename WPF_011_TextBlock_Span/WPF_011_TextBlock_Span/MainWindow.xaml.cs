59b061844dbbb269a8001c3d78de2600b87b94c7
john <jm.kagaya@gmail.com>
2018-03-05 19:18:20 +0900

    Done
    Done

--- WPF_011_TextBlock_Span/WPF_011_TextBlock_Span/MainWindow.xaml.cs
+++ WPF_011_TextBlock_Span/WPF_011_TextBlock_Span/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF_010_TextBlock_Control_Inline_Formatting
+namespace WPF_011_TextBlock_Span
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,15 +23,6 @@ namespace WPF_010_TextBlock_Control_Inline_Formatting
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
-        {
-            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
-            /*
-             Run
-The Run element allows you to style a string using all the available properties of the Span element, but while the Span element may contain other inline elements, a Run element may only contain plain text. This makes the Span element more flexible and therefore the logical choice in most cases.
-             */
         }
     }
 }
