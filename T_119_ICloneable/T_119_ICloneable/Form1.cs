ad07fb6a6161046cc327c375bb8f19a8d4965c71
john <jm.kagaya@gmail.com>
2018-03-08 08:04:21 +0900

    Done
    Done

--- T_119_ICloneable/T_119_ICloneable/Form1.cs
+++ T_119_ICloneable/T_119_ICloneable/Form1.cs
@@ -16,5 +16,32 @@ namespace T_119_ICloneable
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mynames = "adam";
+            string clone = (string)mynames.Clone();
+            MessageBox.Show(clone);
+            Myclass mc = new Myclass();
+            mc.Name = "Kagaya";
+            Myclass cloneclass = (Myclass)mc.Clone();
+            MessageBox.Show(cloneclass.Name);
+        }
+
+    }
+
+    class Myclass : ICloneable
+    {
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public object Clone()
+        {
+            return this;
+        }
     }
 }
