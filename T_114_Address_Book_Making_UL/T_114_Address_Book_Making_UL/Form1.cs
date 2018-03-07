48afc7c580d8e5c456d0358ee20f4183127ce26d
john <jm.kagaya@gmail.com>
2018-03-07 18:24:50 +0900

    Not
    Done

--- T_114_Address_Book_Making_UL/T_114_Address_Book_Making_UL/Form1.cs
+++ T_114_Address_Book_Making_UL/T_114_Address_Book_Making_UL/Form1.cs
@@ -16,7 +16,7 @@ namespace T_114_Address_Book_Making_UL
         {
             InitializeComponent();
         }
-        // part 170
+        // part 170 
         private void button1_Click(object sender, EventArgs e)
         {
 
