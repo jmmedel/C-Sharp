65c323077d540a6edcb84e050a4418b3d1865d64
john <jm.kagaya@gmail.com>
2018-03-07 22:23:09 +0900

    Done
    Done

---
+++ T_115_Notified_When_Files_Change/T_115_Notified_When_Files_Change/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace T_115_Notified_When_Files_Change
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileSystemWatcher fsw = new FileSystemWatcher();
+            fsw.Path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            fsw.NotifyFilter = NotifyFilters.LastWrite;
+            fsw.Filter = "*.txt";
+            fsw.Changed += Fsw_Changed;
+            fsw.EnableRaisingEvents = true;
+        }
+
+        private void Fsw_Changed(object sender, FileSystemEventArgs e)
+        {
+            MessageBox.Show("you have save a text file");
+        }
+    }
+}
