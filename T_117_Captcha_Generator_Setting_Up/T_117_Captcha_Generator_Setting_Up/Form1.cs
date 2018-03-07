356aadf6e9fd63ec2e4c68125146a26ff4fe2fab
john <jm.kagaya@gmail.com>
2018-03-08 07:09:22 +0900

    Done
    Done

--- T_117_Captcha_Generator_Setting_Up/T_117_Captcha_Generator_Setting_Up/Form1.cs
+++ T_117_Captcha_Generator_Setting_Up/T_117_Captcha_Generator_Setting_Up/Form1.cs
@@ -20,42 +20,54 @@ namespace T_117_Captcha_Generator_Setting_Up
         List<string> String = new List<string>();
         Image[] generateCapcha(int amout)
         {
-            Graphics g = panel1.CreateGraphics();
-            g.Clear(panel1.BackColor);
-            Random ran = new Random();
-            SolidBrush b = new SolidBrush(Color.FromArgb(0xFF,ran.Next(0,255), ran.Next(0, 255), ran.Next(0, 255)));
-            Pen p = new Pen(Color.FromArgb(0xFF, ran.Next(0, 255), ran.Next(0, 255), ran.Next(0, 255)));
-            char[] chars = "qwertyuiopasdfghjklzxcvbnm1234567899".ToCharArray();
-            string randomString = "";
-            for (int i = 0; i < 6; i++)
+            Image[] image = new Image[amout];
+            for (int i = 0; i < amout; i++)
             {
-                randomString += chars[ran.Next(0, 35)];
+                Bitmap bitmap = new Bitmap(panel1.Width, panel1.Height);
+                Graphics g = Graphics.FromImage(bitmap);
+                g.Clear(panel1.BackColor);
+                Random ran = new Random();
+                SolidBrush b = new SolidBrush(Color.FromArgb(0xFF, ran.Next(0, 255), ran.Next(0, 255), ran.Next(0, 255)));
+                Pen p = new Pen(Color.FromArgb(0xFF, ran.Next(0, 255), ran.Next(0, 255), ran.Next(0, 255)));
+                char[] chars = "qwertyuiopasdfghjklzxcvbnm1234567899".ToCharArray();
+                string randomString = "";
+                for (int c = 0; i < 6; c++)
+                {
+                    randomString += chars[ran.Next(0, 35)];
+                }
+                byte[] buffer = new byte[randomString.Length];
+                int y = 0;
+                foreach (char c in randomString.ToCharArray())
+                {
+                    buffer[y] = (byte)c;
+                    y++;
+                }
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                string md5string = BitConverter.ToString(md5.ComputeHash(buffer)).Replace("-", "");
+                String.Add(md5string);
+                FontFamily ff = new FontFamily("Arial");
+                Font f = new System.Drawing.Font(ff, 14);
+                g.DrawString(randomString, f, b, 20, 20);
+                for (int z = 0; i < 6; z++)
+                {
+                    int j = ran.Next(0, 2);
+                    if (j == 0) g.DrawRectangle(p, ran.Next(0, 111), ran.Next(0, 60), ran.Next(0, 60), ran.Next(0, 60));
+                    else if (j == 1) g.DrawEllipse(p, ran.Next(0, 111), ran.Next(0, 60), ran.Next(0, 60), ran.Next(0, 60));
+                    p.Color = Color.FromArgb(0xFF, ran.Next(0, 255), ran.Next(0, 255), ran.Next(0, 255));
+                }
+                panel1.BackgroundImage = bitmap;
+                image[i] = bitmap;
             }
-            byte[] buffer = new byte[randomString.Length];
-            int y = 0;
-            foreach (char c in randomString.ToCharArray())
-            {
-                buffer[y] = (byte)c;
-                y++;
-            }
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            string md5string = BitConverter.ToString( md5.ComputeHash(buffer)).Replace("-","");
-            String.Add(md5string);
-            FontFamily ff = new FontFamily("Arial");
-            Font f = new System.Drawing.Font(ff, 14);
-            g.DrawString(randomString, f, b, 20, 20);
-            for (int i = 0; i < 6; i++)
-            {
-                int j = ran.Next(0, 2);
-                if (j == 0) g.DrawRectangle(p, ran.Next(0, 111), ran.Next(0, 60), ran.Next(0, 60), ran.Next(0, 60));
-                else if (j == 1) g.DrawEllipse(p, ran.Next(0, 111), ran.Next(0, 60), ran.Next(0, 60), ran.Next(0, 60));
-                p.Color = Color.FromArgb(0xFF, ran.Next(0, 255), ran.Next(0, 255), ran.Next(0, 255));
-            }
-            return null;
+            return image;
+            
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            generateCapcha(0);
+            foreach (Image item in generateCapcha(5))
+            {
+                MessageBox.Show("Heelo");
+            }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
