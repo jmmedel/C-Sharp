dd32f19b83d5a068a7c702be5e1f50ecc7b43792
john <jm.kagaya@gmail.com>
2018-03-14 12:33:41 +0900

    Done
    Done

--- 022_TCP_IP_Client_And_Server/022_TCP_IP_Client_And_Server/Form1.Designer.cs
+++ 022_TCP_IP_Client_And_Server/022_TCP_IP_Client_And_Server/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btmStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btmStop = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btmStart
+            // 
+            this.btmStart.Location = new System.Drawing.Point(278, 13);
+            this.btmStart.Name = "btmStart";
+            this.btmStart.Size = new System.Drawing.Size(75, 23);
+            this.btmStart.TabIndex = 0;
+            this.btmStart.Text = "Start";
+            this.btmStart.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Host";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(53, 12);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(127, 19);
+            this.txtHost.TabIndex = 2;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(232, 13);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(40, 19);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "8910";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // btmStop
+            // 
+            this.btmStop.Location = new System.Drawing.Point(359, 12);
+            this.btmStop.Name = "btmStop";
+            this.btmStop.Size = new System.Drawing.Size(75, 23);
+            this.btmStop.TabIndex = 5;
+            this.btmStop.Text = "Stop";
+            this.btmStop.UseVisualStyleBackColor = true;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(79, 42);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(355, 193);
+            this.txtStatus.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(479, 260);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btmStop);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btmStart);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btmStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btmStop;
+        private System.Windows.Forms.TextBox txtStatus;
     }
 }
 
