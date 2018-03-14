cb08b42bd3067adb4a8e885cf810adba924d76e0
john <jm.kagaya@gmail.com>
2018-03-14 12:55:06 +0900

    Done
    Done

--- 022_TCP_IP_Client_And_Server/Client/Form1.Designer.cs
+++ 022_TCP_IP_Client_And_Server/Client/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btmConnect = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btmSend = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(242, 22);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(40, 19);
+            this.txtPort.TabIndex = 9;
+            this.txtPort.Text = "8910";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Port";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(63, 21);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(127, 19);
+            this.txtHost.TabIndex = 7;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Host";
+            // 
+            // btmConnect
+            // 
+            this.btmConnect.Location = new System.Drawing.Point(288, 22);
+            this.btmConnect.Name = "btmConnect";
+            this.btmConnect.Size = new System.Drawing.Size(75, 23);
+            this.btmConnect.TabIndex = 5;
+            this.btmConnect.Text = "Connect";
+            this.btmConnect.UseVisualStyleBackColor = true;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(63, 51);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(255, 72);
+            this.txtMessage.TabIndex = 10;
+            // 
+            // btmSend
+            // 
+            this.btmSend.Location = new System.Drawing.Point(288, 129);
+            this.btmSend.Name = "btmSend";
+            this.btmSend.Size = new System.Drawing.Size(75, 23);
+            this.btmSend.TabIndex = 11;
+            this.btmSend.Text = "Send";
+            this.btmSend.UseVisualStyleBackColor = true;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(63, 168);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(255, 120);
+            this.txtStatus.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 325);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btmSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btmConnect);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btmConnect;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btmSend;
+        private System.Windows.Forms.TextBox txtStatus;
     }
 }
 
