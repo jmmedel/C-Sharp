93a2de0964e8e6a9222956f4059f3cd0df930810
john <jm.kagaya@gmail.com>
2018-03-29 03:47:44 +0900

    Done
    Done

--- Design_Pattern_009_Decorator_Structual_code/Design_Pattern_009_Decorator_Structual_code/Program.cs
+++ Design_Pattern_009_Decorator_Structual_code/Design_Pattern_009_Decorator_Structual_code/Program.cs
@@ -3,13 +3,161 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ Decorator
+ Definition
+Attach additional responsibilities to an object dynamically. Decorators provide a flexible alternative to subclassing for extending functionality.
 
-namespace Design_Pattern_009_Decorator_Structual_code
+Frequency of use:Medium
+
+     Participants
+
+    The classes and objects participating in this pattern are:
+
+Component   (LibraryItem)
+defines the interface for objects that can have responsibilities added to them dynamically.
+ConcreteComponent   (Book, Video)
+defines an object to which additional responsibilities can be attached.
+Decorator   (Decorator)
+maintains a reference to a Component object and defines an interface that conforms to Component's interface.
+ConcreteDecorator   (Borrowable)
+adds responsibilities to the component.
+
+
+Structural code in C#
+
+This structural code demonstrates the Decorator pattern which dynamically adds extra functionality to an existing object.
+
+*/
+using System;
+
+namespace DoFactory.GangOfFour.Decorator.Structural
 {
-    class Program
+    /// <summary>
+
+    /// MainApp startup class for Structural 
+
+    /// Decorator Design Pattern.
+
+    /// </summary>
+
+    class MainApp
+
     {
-        static void Main(string[] args)
+        /// <summary>
+
+        /// Entry point into console application.
+
+        /// </summary>
+
+        static void Main()
+        {
+            // Create ConcreteComponent and two Decorators
+
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+            ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+
+            // Link decorators
+
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+
+            d2.Operation();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+
+    /// The 'Component' abstract class
+
+    /// </summary>
+
+    abstract class Component
+
+    {
+        public abstract void Operation();
+    }
+
+    /// <summary>
+
+    /// The 'ConcreteComponent' class
+
+    /// </summary>
+
+    class ConcreteComponent : Component
+
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent.Operation()");
+        }
+    }
+
+    /// <summary>
+
+    /// The 'Decorator' abstract class
+
+    /// </summary>
+
+    abstract class Decorator : Component
+
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+    /// <summary>
+
+    /// The 'ConcreteDecoratorA' class
+
+    /// </summary>
+
+    class ConcreteDecoratorA : Decorator
+
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("ConcreteDecoratorA.Operation()");
+        }
+    }
+
+    /// <summary>
+
+    /// The 'ConcreteDecoratorB' class
+
+    /// </summary>
+
+    class ConcreteDecoratorB : Decorator
+
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("ConcreteDecoratorB.Operation()");
+        }
+
+        void AddedBehavior()
         {
         }
     }
 }
+
