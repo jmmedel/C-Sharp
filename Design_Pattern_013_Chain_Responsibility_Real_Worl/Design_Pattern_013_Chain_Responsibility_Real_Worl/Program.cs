71e6355eed7c8e20167f91dc76efa4cfbc935764
john <jm.kagaya@gmail.com>
2018-03-29 04:47:01 +0900

    Done
    Done

---
+++ Design_Pattern_013_Chain_Responsibility_Real_Worl/Design_Pattern_013_Chain_Responsibility_Real_Worl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_013_Chain_Responsibility_Real_Worl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
