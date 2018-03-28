e3ba5a5267bc97dce18e73616da4cacd2963920e
john <jm.kagaya@gmail.com>
2018-03-29 04:42:04 +0900

    Done
    Done

---
+++ Design_Pattern_012_Proxy_Real_World_Code/Design_Pattern_012_Proxy_Real_World_Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_012_Proxy_Real_World_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
