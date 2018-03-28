da022b363c8f5ff94a2c2e9f44e0651af5801124
john <jm.kagaya@gmail.com>
2018-03-29 01:59:27 +0900

    Done
    Done

---
+++ Design_Pattern_003_Factory_Method_Strcutal_code/Design_Pattern_003_Factory_Method_Strcutal_code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_003_Factory_Method_Strcutal_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
