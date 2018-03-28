4b0b8608236808dcbbdc2363fe7648c2a329aa66
john <jm.kagaya@gmail.com>
2018-03-29 04:55:11 +0900

    Done
    Done

--- Design_Pattern_014_Command_Structural_Code/Design_Pattern_014_Command_Structural_Code/Program.cs
+++ Design_Pattern_014_Command_Structural_Code/Design_Pattern_014_Command_Structural_Code/Program.cs
@@ -11,14 +11,147 @@ using System.Threading.Tasks;
 Encapsulate a request as an object, thereby letting you parameterize clients with different requests, queue or log requests, and support undoable operations.
 
 Frequency of use:Medium high
+Participants
+
+    The classes and objects participating in this pattern are:
+
+Command  (Command)
+declares an interface for executing an operation
+ConcreteCommand  (CalculatorCommand)
+defines a binding between a Receiver object and an action
+implements Execute by invoking the corresponding operation(s) on Receiver
+Client  (CommandApp)
+creates a ConcreteCommand object and sets its receiver
+Invoker  (User)
+asks the command to carry out the request
+Receiver  (Calculator)
+knows how to perform the operations associated with carrying out the request.
+
+
+Structural code in C#
+
+This structural code demonstrates the Command pattern which stores requests as objects allowing clients to execute or playback the requests.
 
 */
-namespace Design_Pattern_014_Command_Structural_Code
+using System;
+
+namespace DoFactory.GangOfFour.Command.Structural
 {
-    class Program
+    /// <summary>
+
+    /// MainApp startup class for Structural 
+
+    /// Command Design Pattern.
+
+    /// </summary>
+
+    class MainApp
+
     {
-        static void Main(string[] args)
+        /// <summary>
+
+        /// Entry point into console application.
+
+        /// </summary>
+
+        static void Main()
         {
+            // Create receiver, command, and invoker
+
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
+
+            // Set and execute command
+
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+
+    /// The 'Command' abstract class
+
+    /// </summary>
+
+    abstract class Command
+
+    {
+        protected Receiver receiver;
+
+        // Constructor
+
+        public Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+
+    /// <summary>
+
+    /// The 'ConcreteCommand' class
+
+    /// </summary>
+
+    class ConcreteCommand : Command
+
+    {
+        // Constructor
+
+        public ConcreteCommand(Receiver receiver) :
+          base(receiver)
+        {
+        }
+
+        public override void Execute()
+        {
+            receiver.Action();
+        }
+    }
+
+    /// <summary>
+
+    /// The 'Receiver' class
+
+    /// </summary>
+
+    class Receiver
+
+    {
+        public void Action()
+        {
+            Console.WriteLine("Called Receiver.Action()");
+        }
+    }
+
+    /// <summary>
+
+    /// The 'Invoker' class
+
+    /// </summary>
+
+    class Invoker
+
+    {
+        private Command _command;
+
+        public void SetCommand(Command command)
+        {
+            this._command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            _command.Execute();
         }
     }
 }
+
+
