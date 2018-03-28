9887bf5fd4c7dec486d12fe63d669c35bf64cce8
john <jm.kagaya@gmail.com>
2018-03-29 04:50:35 +0900

    Done
    Done

--- Design_Pattern_014_Command_Structural_Code/Design_Pattern_014_Command_Structural_Code/Program.cs
+++ Design_Pattern_014_Command_Structural_Code/Design_Pattern_014_Command_Structural_Code/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * 
+ Command
+ Definition
+Encapsulate a request as an object, thereby letting you parameterize clients with different requests, queue or log requests, and support undoable operations.
+
+Frequency of use:Medium high
+
+*/
 namespace Design_Pattern_014_Command_Structural_Code
 {
     class Program
