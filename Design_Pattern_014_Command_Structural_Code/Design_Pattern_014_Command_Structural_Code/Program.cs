32e98232861bdf510ce32570b50656556011490f
john <jm.kagaya@gmail.com>
2018-03-29 04:49:19 +0900

    Done
    Done

---
+++ Design_Pattern_014_Command_Structural_Code/Design_Pattern_014_Command_Structural_Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_014_Command_Structural_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
