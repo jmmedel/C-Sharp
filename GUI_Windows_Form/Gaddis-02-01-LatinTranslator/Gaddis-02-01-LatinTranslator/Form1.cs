25dd71407ab0d4360306c01ae77126e0542c5369
kagaya25 <jm.kagaya@gmail.com>
2018-05-04 08:41:01 +0900

    Done
    Done

---
+++ GUI_Windows_Form/Gaddis-02-01-LatinTranslator/Gaddis-02-01-LatinTranslator/Form1.cs
@@ -0,0 +1,44 @@
+﻿/*
+Latin Translator Look at the following list of Latin words and their meanings. 
+Latin sinister - dexter - medium 
+English left - right - center
+Create an application that translates the Latin words to English. 
+The form should have three buttons, one for each Latin word. 
+When the user clicks a button, the application should display the English translation in a Label control.
+*/
+using System;
+using System.Windows.Forms;
+
+namespace Gaddis_02_01_LatinTranslator
+{
+  public partial class frmLatinTranslator : Form
+  {
+    public frmLatinTranslator()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnLatinSinister_Click(object sender, EventArgs e)
+    {
+      lblOutput.Text = "Left";
+      lblOutput.Left = 50;
+    }
+
+    private void btnLatinMedium_Click(object sender, EventArgs e)
+    {
+      lblOutput.Text = "Center";
+      lblOutput.Left = (this.ClientSize.Width -lblOutput.Width) / 2;
+    }
+
+    private void btnLatinDexter_Click(object sender, EventArgs e)
+    {
+      lblOutput.Text = "Right";
+      lblOutput.Left = 240;
+    }
+  }
+}
