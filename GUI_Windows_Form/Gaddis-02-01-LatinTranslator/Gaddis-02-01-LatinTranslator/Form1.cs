1377695cd4da2c2462782b0419d0045462fe1988
kagaya25 <jm.kagaya@gmail.com>
2018-05-05 11:39:19 +0900

    Done
    Done

--- GUI_Windows_Form/Gaddis-02-01-LatinTranslator/Gaddis-02-01-LatinTranslator/Form1.cs
+++ GUI_Windows_Form/Gaddis-02-01-LatinTranslator/Gaddis-02-01-LatinTranslator/Form1.cs
@@ -22,25 +22,25 @@ namespace Gaddis_02_01_LatinTranslator
     {
       InitializeComponent();
     }
-
+        //load
     private void Form1_Load(object sender, EventArgs e)
     {
 
     }
-
+        //clickbutton
     private void btnLatinSinister_Click(object sender, EventArgs e)
     {
       lblOutput.Text = "Left";
       lblOutput.Left = 50;
     }
-
-    private void btnLatinMedium_Click(object sender, EventArgs e)
+        //clickbutton
+        private void btnLatinMedium_Click(object sender, EventArgs e)
     {
       lblOutput.Text = "Center";
       lblOutput.Left = (this.ClientSize.Width -lblOutput.Width) / 2;
     }
-
-    private void btnLatinDexter_Click(object sender, EventArgs e)
+        //clickbutton
+        private void btnLatinDexter_Click(object sender, EventArgs e)
     {
       lblOutput.Text = "Right";
       lblOutput.Left = 240;
