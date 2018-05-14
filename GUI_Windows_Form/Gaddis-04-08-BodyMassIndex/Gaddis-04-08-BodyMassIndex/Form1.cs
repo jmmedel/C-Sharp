1713784dd9c35af96d577fee6ff31d8893c9f39f
kagaya25 <jm.kagaya@gmail.com>
2018-05-14 11:25:09 +0900

    Done
    Done

--- GUI_Windows_Form/Gaddis-04-08-BodyMassIndex/Gaddis-04-08-BodyMassIndex/Form1.cs
+++ GUI_Windows_Form/Gaddis-04-08-BodyMassIndex/Gaddis-04-08-BodyMassIndex/Form1.cs
@@ -1,4 +1,5 @@
 ﻿/*
+ * Done
 A person’s BMI is calculated with the following formula: BMI = Weight × 703 ÷ Height(power of 2) In the formula, weight is measured in pounds and height is measured in inches. Enhance the program so it displays a message indicating whether the person has optimal weight, is underweight, or is overweight.A person’s weight is considered to be optimal if his or her BMI is between 18.5 and 25. If the BMI is less than 18.5, the person is considered to be underweight. If the BMI value is greater than 25, the person is considered to be overweight.
 */
 
