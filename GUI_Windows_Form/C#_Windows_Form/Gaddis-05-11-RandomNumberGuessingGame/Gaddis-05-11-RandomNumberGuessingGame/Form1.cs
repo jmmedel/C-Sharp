491c96530dcc3eff4807f58352adbc55dc4ef75a
kagaya25 <jm.kagaya@gmail.com>
2018-05-20 09:39:56 +0900

    Done
    Done

--- GUI_Windows_Form/C#_Windows_Form/Gaddis-05-11-RandomNumberGuessingGame/Gaddis-05-11-RandomNumberGuessingGame/Form1.cs
+++ GUI_Windows_Form/C#_Windows_Form/Gaddis-05-11-RandomNumberGuessingGame/Gaddis-05-11-RandomNumberGuessingGame/Form1.cs
@@ -1,4 +1,5 @@
 ﻿/*
+ * Done
 Random Number Guessing Game Create an application that generates a random number in the range of 1 through 100 and asks the user to guess what the number is. If the user’s guess is higher than the random number, the program should display “Too high, try again.” If the user’s guess is lower than the random number, the program should display “Too low, try again.” If the user guesses the number, the application should congratulate the user and then generate a new random number so the game can start over.Optional Enhancement: Enhance the game so it keeps count of the number of guesses that the user makes. When the user correctly guesses the random number, the program should display the number of guesses.
 */
 using System;
